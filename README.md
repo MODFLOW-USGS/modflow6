9c524397d5fabbc869408d0fee14232379a5e96a
Christian D Langevin <langevin@usgs.gov>
2018-01-05 16:28:06 -0500

    Removed a bunch of the latex formatting from the input variable descriptions.

--- README.md
+++ README.md
@@ -7,7 +7,7 @@
 
 ## Automated Testing Status on Travis-CI
 
-### Version 6.0.1 develop &mdash; build 3
+### Version 6.0.1 develop &mdash; build 4
 [![Build Status](https://travis-ci.org/MODFLOW-USGS/modflow6.svg?branch=develop)](https://travis-ci.org/MODFLOW-USGS/modflow6)
 
 ## Introduction
--- doc/version.py
+++ doc/version.py
@@ -1,11 +1,11 @@
 # MODFLOW 6 version file automatically created using...pre-commit.py
-# created on...January 05, 2018 15:29:49
+# created on...January 05, 2018 16:28:06
 
 major = 6
 minor = 0
 micro = 1
-build = 3
-commit = 5
+build = 4
+commit = 6
 
 __version__ = '{:d}.{:d}.{:d}'.format(major, minor, micro)
 __build__ = '{:d}.{:d}.{:d}.{:d}'.format(major, minor, micro, build)
