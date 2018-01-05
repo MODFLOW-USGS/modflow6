c06896535f09d91dbb6f010609241e9688933344
Joseph D Hughes <jdhughes@usgs.gov>
2018-01-05 18:17:30 -0500

    Updates to update_flopy.py script to diagnose issue with travis

--- doc/version.py
+++ doc/version.py
@@ -1,11 +1,11 @@
 # MODFLOW 6 version file automatically created using...pre-commit.py
-# created on...January 05, 2018 16:44:19
+# created on...January 05, 2018 18:17:30
 
 major = 6
 minor = 0
 micro = 1
-build = 5
-commit = 7
+build = 6
+commit = 8
 
 __version__ = '{:d}.{:d}.{:d}'.format(major, minor, micro)
 __build__ = '{:d}.{:d}.{:d}.{:d}'.format(major, minor, micro, build)
