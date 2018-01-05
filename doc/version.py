e002ab81d77c7078c7fef1f8f2735b7a6fe9112d
Joseph D Hughes <jdhughes@usgs.gov>
2018-01-05 16:44:19 -0500

    Updates to autotest suite

--- doc/version.py
+++ doc/version.py
@@ -1,11 +1,11 @@
 # MODFLOW 6 version file automatically created using...pre-commit.py
-# created on...January 05, 2018 16:28:06
+# created on...January 05, 2018 16:44:19
 
 major = 6
 minor = 0
 micro = 1
-build = 4
-commit = 6
+build = 5
+commit = 7
 
 __version__ = '{:d}.{:d}.{:d}'.format(major, minor, micro)
 __build__ = '{:d}.{:d}.{:d}.{:d}'.format(major, minor, micro, build)
