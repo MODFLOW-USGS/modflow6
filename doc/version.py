a173f7cb7849a0c06097d3e3f2247908995162fe
Christian D Langevin <langevin@usgs.gov>
2018-01-08 13:32:08 -0500

    Made a few corrections to input instructions.

--- doc/version.py
+++ doc/version.py
@@ -1,11 +1,11 @@
 # MODFLOW 6 version file automatically created using...pre-commit.py
-# created on...January 05, 2018 18:17:30
+# created on...January 08, 2018 13:32:08
 
 major = 6
 minor = 0
 micro = 1
-build = 6
-commit = 8
+build = 7
+commit = 9
 
 __version__ = '{:d}.{:d}.{:d}'.format(major, minor, micro)
 __build__ = '{:d}.{:d}.{:d}.{:d}'.format(major, minor, micro, build)
