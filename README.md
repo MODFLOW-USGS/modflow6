cd4c2340b279eed85b30120e27727e5b80849621
Christian D Langevin <langevin@usgs.gov>
2018-01-08 15:21:52 -0500

    Correcting a mistake I made.

--- README.md
+++ README.md
@@ -7,7 +7,7 @@
 
 ## Automated Testing Status on Travis-CI
 
-### Version 6.0.1 develop &mdash; build 8
+### Version 6.0.1 develop &mdash; build 9
 [![Build Status](https://travis-ci.org/MODFLOW-USGS/modflow6.svg?branch=develop)](https://travis-ci.org/MODFLOW-USGS/modflow6)
 
 ## Introduction
--- doc/version.py
+++ doc/version.py
@@ -1,11 +1,11 @@
 # MODFLOW 6 version file automatically created using...pre-commit.py
-# created on...January 08, 2018 15:06:24
+# created on...January 08, 2018 15:21:52
 
 major = 6
 minor = 0
 micro = 1
-build = 8
-commit = 10
+build = 9
+commit = 11
 
 __version__ = '{:d}.{:d}.{:d}'.format(major, minor, micro)
 __build__ = '{:d}.{:d}.{:d}.{:d}'.format(major, minor, micro, build)
