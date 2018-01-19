d772165ae6f27893278c0781e272381f8aad769b
Joseph D Hughes <jdhughes@usgs.gov>
2018-01-19 12:44:00 -0500

    Updated autotest scripts with modified scripts from subsidence branch

--- doc/version.py
+++ doc/version.py
@@ -1,11 +1,11 @@
 # MODFLOW 6 version file automatically created using...pre-commit.py
-# created on...January 13, 2018 20:04:06
+# created on...January 19, 2018 12:44:01
 
 major = 6
 minor = 0
 micro = 1
-build = 10
-commit = 12
+build = 12
+commit = 14
 
 __version__ = '{:d}.{:d}.{:d}'.format(major, minor, micro)
 __build__ = '{:d}.{:d}.{:d}.{:d}'.format(major, minor, micro, build)
