18b68cecf2e0510efafd10d6a73f30f5bb46885a
Christian D Langevin <langevin@usgs.gov>
2018-02-06 10:57:45 -0500

    As part of RP, MAW would check the simulated head in every well and compare it will the well bottom.  The intent of this check is to see if a user specified MAW head is below the cell bottom.  But the check was being done for every well no matter what.  The changes here now just check a user provided head with the cell bottom.  So this way, if a simulated head is below the well bottom, the program will not terminate with an error.  This may be useful is the user is continuing even without convergence.

--- doc/version.py
+++ doc/version.py
@@ -1,11 +1,11 @@
 # MODFLOW 6 version file automatically created using...pre-commit.py
-# created on...February 03, 2018 19:46:00
+# created on...February 06, 2018 10:57:45
 
 major = 6
 minor = 0
 micro = 1
-build = 15
-commit = 17
+build = 16
+commit = 18
 
 __version__ = '{:d}.{:d}.{:d}'.format(major, minor, micro)
 __build__ = '{:d}.{:d}.{:d}.{:d}'.format(major, minor, micro, build)
