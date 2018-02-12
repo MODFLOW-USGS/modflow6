ed4b9afa77f3c2aec4337e0fb4bf76165f3c6d06
Christian D Langevin <langevin@usgs.gov>
2018-02-12 16:24:11 -0500

    Updated the python makefile

--- README.md
+++ README.md
@@ -7,7 +7,7 @@
 
 ## Automated Testing Status on Travis-CI
 
-### Version 6.0.1 develop &mdash; build 22
+### Version 6.0.1 develop &mdash; build 23
 [![Build Status](https://travis-ci.org/MODFLOW-USGS/modflow6.svg?branch=develop)](https://travis-ci.org/MODFLOW-USGS/modflow6)
 
 ## Introduction
--- doc/version.py
+++ doc/version.py
@@ -1,11 +1,11 @@
 # MODFLOW 6 version file automatically created using...pre-commit.py
-# created on...February 12, 2018 10:00:29
+# created on...February 12, 2018 16:24:11
 
 major = 6
 minor = 0
 micro = 1
-build = 22
-commit = 24
+build = 23
+commit = 25
 
 __version__ = '{:d}.{:d}.{:d}'.format(major, minor, micro)
 __build__ = '{:d}.{:d}.{:d}.{:d}'.format(major, minor, micro, build)
