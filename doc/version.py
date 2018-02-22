14271e0d54e52d76b2928fdf9c07ca5a5f213e3a
Christian D Langevin <langevin@usgs.gov>
2018-02-22 10:34:02 -0500

    Updated release notes to reflect the Intel compiler version.

--- doc/version.py
+++ doc/version.py
@@ -1,11 +1,11 @@
 # MODFLOW 6 version file automatically created using...pre-commit.py
-# created on...February 20, 2018 15:19:40
+# created on...February 22, 2018 10:34:02
 
 major = 6
 minor = 0
 micro = 1
-build = 33
-commit = 35
+build = 34
+commit = 36
 
 __version__ = '{:d}.{:d}.{:d}'.format(major, minor, micro)
 __build__ = '{:d}.{:d}.{:d}.{:d}'.format(major, minor, micro, build)
