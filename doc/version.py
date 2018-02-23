237073ef96133ea36f28527bb9bb4fdbaa62f0c2
Christian D Langevin <langevin@usgs.gov>
2018-02-23 08:04:43 -0500

    The description of specific discharge calculation was added to mf6io.

--- doc/version.py
+++ doc/version.py
@@ -1,11 +1,11 @@
 # MODFLOW 6 version file automatically created using...pre-commit.py
-# created on...February 22, 2018 10:34:02
+# created on...February 23, 2018 08:04:43
 
 major = 6
 minor = 0
 micro = 1
-build = 34
-commit = 36
+build = 35
+commit = 37
 
 __version__ = '{:d}.{:d}.{:d}'.format(major, minor, micro)
 __build__ = '{:d}.{:d}.{:d}.{:d}'.format(major, minor, micro, build)
