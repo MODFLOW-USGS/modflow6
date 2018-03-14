a6cd930ac287b683919edcf859652983f90358e9
Joseph D Hughes <jdhughes@usgs.gov>
2018-03-14 13:22:31 -0400

    Added two multi-model models. One tests (testmf6_npf02_rewet.py) rewetting for a single model and two models for a single and 3 layer discretization. The other (testmf6_npf03_sfr.py) tests a single and two model version of model with SFR and MAW with a mover.

--- doc/version.py
+++ doc/version.py
@@ -1,11 +1,11 @@
 # MODFLOW 6 version file automatically created using...pre-commit.py
-# created on...February 23, 2018 08:04:43
+# created on...March 14, 2018 13:22:31
 
 major = 6
 minor = 0
 micro = 1
-build = 35
-commit = 37
+build = 36
+commit = 38
 
 __version__ = '{:d}.{:d}.{:d}'.format(major, minor, micro)
 __build__ = '{:d}.{:d}.{:d}.{:d}'.format(major, minor, micro, build)
