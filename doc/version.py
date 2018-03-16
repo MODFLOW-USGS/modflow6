9e7a35ac2a31bfd460050b93c91976044e4f945a
Joseph D Hughes <jdhughes@usgs.gov>
2018-03-16 18:26:25 -0400

    Update to readme file

--- doc/version.py
+++ doc/version.py
@@ -1,11 +1,11 @@
 # MODFLOW 6 version file automatically created using...pre-commit.py
-# created on...March 16, 2018 18:18:33
+# created on...March 16, 2018 18:26:25
 
 major = 6
 minor = 0
-micro = 1
-build = 66
-commit = 68
+micro = 2
+build = 33
+commit = 69
 
 __version__ = '{:d}.{:d}.{:d}'.format(major, minor, micro)
 __build__ = '{:d}.{:d}.{:d}.{:d}'.format(major, minor, micro, build)
