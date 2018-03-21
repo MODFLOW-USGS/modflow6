8d62280eb3f7c1a7d9e849d06dadc3e4f3ec3790
Christian D Langevin <langevin@usgs.gov>
2018-03-21 10:58:15 -0400

    Slight MAW dfn change.

--- doc/version.py
+++ doc/version.py
@@ -1,11 +1,11 @@
 # MODFLOW 6 version file automatically created using...pre-commit.py
-# created on...March 20, 2018 17:01:07
+# created on...March 21, 2018 10:58:15
 
 major = 6
 minor = 0
 micro = 1
-build = 40
-commit = 42
+build = 41
+commit = 43
 
 __version__ = '{:d}.{:d}.{:d}'.format(major, minor, micro)
 __build__ = '{:d}.{:d}.{:d}.{:d}'.format(major, minor, micro, build)
