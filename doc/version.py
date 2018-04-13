f3fb7791a898172448b4561413169253efc64863
Joseph D Hughes <jdhughes@usgs.gov>
2018-04-13 11:27:25 -0400

    Added usgs.code.gov files and update pre-commit.py script used by precommit hooks to automatically generate the appropriate disclaimer and update code.json

--- doc/version.py
+++ doc/version.py
@@ -1,11 +1,11 @@
 # MODFLOW 6 version file automatically created using...pre-commit.py
-# created on...April 06, 2018 08:42:17
+# created on...April 13, 2018 11:27:25
 
 major = 6
 minor = 0
-micro = 1
-build = 42
-commit = 44
+micro = 2
+build = 9
+commit = 45
 
 __version__ = '{:d}.{:d}.{:d}'.format(major, minor, micro)
 __build__ = '{:d}.{:d}.{:d}.{:d}'.format(major, minor, micro, build)
