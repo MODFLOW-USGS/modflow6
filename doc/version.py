80e59507bd496f360d01832c0163f5404b2f1aac
Joseph D Hughes <jdhughes@usgs.gov>
2018-04-23 15:22:07 -0400

    Fixed issue related to preconditioner work array allocation when the number of levels is zero but a non-zero drop tolerance is specified. Tested against problem that detected issue (from aquaveo). Updated ims output and value evaluation.

--- doc/version.py
+++ doc/version.py
@@ -1,11 +1,11 @@
 # MODFLOW 6 version file automatically created using...pre-commit.py
-# created on...April 20, 2018 12:27:02
+# created on...April 23, 2018 15:22:07
 
 major = 6
 minor = 0
 micro = 2
-build = 11
-commit = 47
+build = 12
+commit = 48
 
 __version__ = '{:d}.{:d}.{:d}'.format(major, minor, micro)
 __build__ = '{:d}.{:d}.{:d}.{:d}'.format(major, minor, micro, build)
