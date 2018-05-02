8f8f3f250779f0f62b3367389159aa63133e01cb
Christian D Langevin <langevin@usgs.gov>
2018-05-02 15:15:44 -0400

    Started adding all the latex files for the transport model.

--- doc/version.py
+++ doc/version.py
@@ -1,11 +1,11 @@
 # MODFLOW 6 version file automatically created using...pre-commit.py
-# created on...May 02, 2018 14:08:17
+# created on...May 02, 2018 15:15:45
 
 major = 6
 minor = 0
 micro = 2
-build = 52
-commit = 88
+build = 53
+commit = 89
 
 __version__ = '{:d}.{:d}.{:d}'.format(major, minor, micro)
 __build__ = '{:d}.{:d}.{:d}.{:d}'.format(major, minor, micro, build)
