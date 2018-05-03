e218e0cdb529dcd7486679c3e12f6aaa134e1d52
Christian D Langevin <langevin@usgs.gov>
2018-05-03 08:33:48 -0400

    Little more on gwt user guide.

--- doc/version.py
+++ doc/version.py
@@ -1,11 +1,11 @@
 # MODFLOW 6 version file automatically created using...pre-commit.py
-# created on...May 02, 2018 17:51:17
+# created on...May 03, 2018 08:33:49
 
 major = 6
 minor = 0
 micro = 2
-build = 54
-commit = 90
+build = 55
+commit = 91
 
 __version__ = '{:d}.{:d}.{:d}'.format(major, minor, micro)
 __build__ = '{:d}.{:d}.{:d}.{:d}'.format(major, minor, micro, build)
