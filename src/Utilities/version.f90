aca7d35994cd1f41463a0ef7a5ddae1e9d98a279
Christian D Langevin <langevin@usgs.gov>
2018-10-14 14:55:22 -0500

    refactor(pre-commit.py): use OrderdedDict to load and write code.json
    Updated the code.json writer to use an ordered dictionary so that the order is always the same.  This will minimize the number of changes that appear in the changelog due to ordering changes that do not change content.

--- src/Utilities/version.f90
+++ src/Utilities/version.f90
@@ -3,7 +3,7 @@ module VersionModule
   public
   ! -- modflow 6 version
   integer(I4B), parameter :: IDEVELOPMODE = 1
-  character(len=40), parameter :: VERSION = '6.0.3.24 10/01/2018'
+  character(len=40), parameter :: VERSION = '6.0.3.25 10/14/2018'
   character(len=10), parameter :: MFVNAM = ' 6'
   character(len=*), parameter  :: MFTITLE =                                     &
     'U.S. GEOLOGICAL SURVEY MODULAR HYDROLOGIC MODEL'
