22939d46c21079737428e0d1987a6f845014bb51
jdhughes-usgs <jdhughes@usgs.gov>
2018-12-03 15:05:24 -0500

    csub(feat): convergence check with defined precision
    Modified csub convergence check to evaluate a fixed number of
    significant digits (8) instead of a defined percent difference. The
    defined percent difference had issues with the delay bed flows were
    small (< 1e-10).

--- src/Utilities/version.f90
+++ src/Utilities/version.f90
@@ -3,7 +3,7 @@ module VersionModule
   public
   ! -- modflow 6 version
   integer(I4B), parameter :: IDEVELOPMODE = 1
-  character(len=40), parameter :: VERSION = '6.0.2.199 11/01/2018'
+  character(len=40), parameter :: VERSION = '6.0.2.203 12/03/2018'
   character(len=10), parameter :: MFVNAM = ' 6'
   character(len=*), parameter  :: MFTITLE =                                     &
     'U.S. GEOLOGICAL SURVEY MODULAR HYDROLOGIC MODEL'
