9ee1bf05452bebf1c15d4e3aed117706dac9cd89
Hughes, J.D <jdhughes@usgs.gov>
2019-04-05 15:31:37 -0400

    feat(ims): Add no_ptc_option FIRST and ALL to no_ptc options keyword (#134)
    Add no_ptc_option FIRST and ALL to no_ptc options keyword. Add autotest
    (test_gwf_noptc01.py) to test new option against simulation with PTC.

--- autotest/test_gwf_noptc01.py
+++ autotest/test_gwf_noptc01.py
@@ -1,5 +1,5 @@
 import os
-from nose.tools import *
+import numpy as np
 
 try:
     import flopy
@@ -10,48 +10,72 @@ except:
     raise Exception(msg)
 
 from framework import testing_framework
-import targets
+from simulation import Simulation
+
+ex = ['gwf_noptc01', 'gwf_noptc02', 'gwf_noptc03']
+exdirs = []
+for s in ex:
+    exdirs.append(os.path.join('temp', s))
+
+no_ptcrecords = ['FIRST', 'ALL', '']
+
+ddir = 'data'
+
+## run all examples on Travis
+travis = [True for idx in range(len(exdirs))]
+
+# set replace_exe to None to use default executable
+# replace_exe = {'mf2005': 'mf2005devdbl'}
+replace_exe = None
+
+htol = [None for idx in range(len(exdirs))]
+
+# static model data
+# temporal discretization
+nper = 1
+tdis_rc = [(1., 1, 1.0)]
+
+# spatial discretization data
+nlay, nrow, ncol = 1, 1, 100
+shape3d = (nlay, nrow, ncol)
+size3d = nlay * nrow * ncol
+delr, delc = 50., 1.
+top = 25.
+botm = 0.
+strt = 0.
+
+# hydraulic properties
+hk = 50.
+
+# all cells are active and layer 1 is convertible
+ib = 1
+
+# solver options
+nouter, ninner = 500, 300
+hclose, rclose, relax = 1e-9, 1e-6, 1.
+newtonoptions = ''
+imsla = 'BICGSTAB'
+
+# chd data
+c6 = []
+ccol = [0, ncol - 1]
+hc = [20., 11.]
+for j, h in zip(ccol, hc):
+    c6.append([(0, 0, j), h])
+cd6 = {0: c6}
+maxchd = len(cd6[0])
+
+# recharge data
+rech = {0: 0.001}
 
 
-def get_model():
-    name = 'csub_idomain01'
-    mf6_exe = os.path.abspath(targets.target_dict['mf6'])
-
-    # static model data
-    # temporal discretization
-    nper = 1
-    tdis_rc = []
-    for idx in range(nper):
-        tdis_rc.append((1., 1, 1.0))
-
-    # spatial discretization data
-    nlay, nrow, ncol = 1, 10, 10
-    delr, delc = 1000., 1000.
-    top = 0.
-    botm = [-100]
-    strt = 0.
-
-    # calculate hk
-    hk = 1.
-
-    # solver options
-    nouter, ninner = 500, 300
-    hclose, rclose, relax = 1e-9, 1e-6, 1.
-    newtonoptions = ''
-    imsla = 'BICGSTAB'
-
-    # chd data
-    c6 = []
-    ccol = [1, ncol - 1]
-    for j in ccol:
-        c6.append([(0, nrow - 1, j), strt])
-    cd6 = {0: c6}
-    maxchd = len(cd6[0])
+def build_model(idx, dir, no_ptcrecord):
+    name = ex[idx]
 
     # build MODFLOW 6 files
-    ws = os.path.join('temp', name)
+    ws = dir
     sim = flopy.mf6.MFSimulation(sim_name=name, version='mf6',
-                                 exe_name=mf6_exe,
+                                 exe_name='mf6',
                                  sim_ws=ws)
     # create tdis package
     tdis = flopy.mf6.ModflowTdis(sim, time_units='DAYS',
@@ -64,6 +88,7 @@ def get_model():
 
     # create iterative model solution and register the gwf model with it
     ims = flopy.mf6.ModflowIms(sim, print_option='SUMMARY',
+                               no_ptcrecord=no_ptcrecord,
                                outer_hclose=hclose,
                                outer_maximum=nouter,
                                under_relaxation='NONE',
@@ -77,7 +102,7 @@ def get_model():
 
     dis = flopy.mf6.ModflowGwfdis(gwf, nlay=nlay, nrow=nrow, ncol=ncol,
                                   delr=delr, delc=delc,
-                                  top=top, botm=botm, idomain=0)
+                                  top=top, botm=botm)
 
     # initial conditions
     ic = flopy.mf6.ModflowGwfic(gwf, strt=strt)
@@ -86,6 +111,9 @@ def get_model():
     npf = flopy.mf6.ModflowGwfnpf(gwf, save_flows=False,
                                   icelltype=1,
                                   k=hk)
+
+    # recharge
+    rch = flopy.mf6.ModflowGwfrcha(gwf, readasarrays=True, recharge=rech)
 
     # chd files
     chd = flopy.mf6.modflow.mfgwfchd.ModflowGwfchd(gwf,
@@ -108,26 +136,60 @@ def get_model():
     return sim
 
 
-@raises(AssertionError)
-def run_mf6():
-    sim = get_model()
-    sim.write_simulation()
-    success, buff = sim.run_simulation()
-    msg = 'could not run {}'.format(sim.name)
-    if not success:
-        raise AssertionError(msg)
+# water table recharge problem
+def get_model(idx, dir):
+    sim = build_model(idx, dir, no_ptcrecords[idx])
+
+    # build MODFLOW-6 without no_ptc option
+    pth = os.path.join(dir, 'mf6')
+    mc = build_model(idx, pth, None)
+
+    return sim, mc
+
+
+def build_models():
+    for idx, dir in enumerate(exdirs):
+        sim, mc = get_model(idx, dir)
+        sim.write_simulation()
+        mc.write_simulation()
     return
 
 
+# - No need to change any code below
 def test_mf6model():
+    # determine if running on Travis
+    is_travis = 'TRAVIS' in os.environ
+    r_exe = None
+    if not is_travis:
+        if replace_exe is not None:
+            r_exe = replace_exe
+
+    # initialize testing framework
+    test = testing_framework()
+
+    # build the models
+    build_models()
+
     # run the test models
-    yield run_mf6
+    for idx, dir in enumerate(exdirs):
+        if is_travis and not travis[idx]:
+            continue
+        yield test.run_mf6, Simulation(dir, idxsim=idx)
 
     return
 
 
 def main():
-    run_mf6()
+    # initialize testing framework
+    test = testing_framework()
+
+    # build the models
+    build_models()
+
+    # run the test models
+    for idx, dir in enumerate(exdirs):
+        sim = Simulation(dir, idxsim=idx)
+        test.run_mf6(sim)
 
     return
 
