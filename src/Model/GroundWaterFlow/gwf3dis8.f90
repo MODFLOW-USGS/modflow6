65be4ba299179668034d215ed165c036ac7a5e5a
jdhughes-usgs <jdhughes@usgs.gov>
2019-09-18 12:35:54 -0400

    refactor(griddata): refactor grid data to isolate read and constructor

--- src/Model/GroundWaterFlow/gwf3dis8.f90
+++ src/Model/GroundWaterFlow/gwf3dis8.f90
@@ -23,7 +23,9 @@ module GwfDisModule
     integer(I4B), dimension(:), pointer, contiguous :: nodeuser => null()        ! (size:nodes) given a reduced nodenumber, provide the user nodenumber (size 0 if not reduced)
     real(DP), dimension(:), pointer, contiguous :: delr => null()                ! spacing along a row
     real(DP), dimension(:), pointer, contiguous :: delc => null()                ! spacing along a column
-    real(DP), dimension(:, :, :), pointer, contiguous :: botm => null()          ! top and bottom elevations for each cell (ncol, nrow, nlay)
+    !real(DP), dimension(:, :, :), pointer, contiguous :: botm => null()          ! top and bottom elevations for each cell (ncol, nrow, nlay)
+    real(DP), dimension(:, :), pointer, contiguous :: top2d => null()            ! top elevations for each cell at top of model (ncol, nrow)
+    real(DP), dimension(:, :, :), pointer, contiguous :: bot3d => null()         ! bottom elevations for each cell (ncol, nrow, nlay)
     integer(I4B), dimension(:, :, :), pointer, contiguous :: idomain => null()   ! idomain (ncol, nrow, nlay)
   contains
     procedure :: dis_df => dis3d_df
@@ -143,7 +145,9 @@ module GwfDisModule
     ! -- Deallocate Arrays
     call mem_deallocate(this%nodereduced)
     call mem_deallocate(this%nodeuser)
-    call mem_deallocate(this%botm)
+    call mem_deallocate(this%top2d)
+    call mem_deallocate(this%bot3d)
+    !call mem_deallocate(this%botm)
     call mem_deallocate(this%idomain)
     !
     ! -- Return
@@ -328,6 +332,7 @@ module GwfDisModule
     integer(I4B) :: ierr
     integer(I4B) :: nrsize, nvals
     logical :: isfound, endOfBlock
+    real(DP) :: top
     real(DP) :: dz
     integer(I4B), parameter :: nname = 5
     logical, dimension(nname) :: lname
@@ -354,7 +359,10 @@ module GwfDisModule
     call mem_allocate(this%delc, this%nrow, 'DELC', this%origin)
     call mem_allocate(this%idomain, this%ncol, this%nrow, this%nlay, 'IDOMAIN',  &
                       this%origin)
-    call mem_allocate(this%botm, this%ncol, this%nrow, this%nlay + 1, 'BOTM',    &
+    !call mem_allocate(this%botm, this%ncol, this%nrow, this%nlay + 1, 'BOTM',    &
+    !                  this%origin)
+    call mem_allocate(this%top2d, this%ncol, this%nrow, 'TOP2D', this%origin)
+    call mem_allocate(this%bot3d, this%ncol, this%nrow, this%nlay, 'BOT3D',      &
                       this%origin)
     !
     ! --Read GRIDDATA block
@@ -376,18 +384,25 @@ module GwfDisModule
                             this%ndim, this%nrow, this%iout, 0)
             lname(2) = .true.
           case ('TOP')
-            call ReadArray(this%parser%iuactive, this%botm(:,:,1), aname(3), &
+            !call ReadArray(this%parser%iuactive, this%botm(:,:,1), aname(3),     &
+            !                this%ndim, this%ncol, this%nrow, this%iout, 0)
+            call ReadArray(this%parser%iuactive, this%top2d(:,:), aname(3),      &
                             this%ndim, this%ncol, this%nrow, this%iout, 0)
             lname(3) = .true.
           case ('BOTM')
             call this%parser%GetStringCaps(keyword)
             if (keyword .EQ. 'LAYERED') then
-              call ReadArray(this%parser%iuactive, this%botm(:,:,2:this%nlay+1), &
-                              aname(4), this%ndim, this%ncol, this%nrow, &
+              !call ReadArray(this%parser%iuactive, this%botm(:,:,2:this%nlay+1), &
+              !                aname(4), this%ndim, this%ncol, this%nrow, &
+              !                this%nlay, this%iout, 1, this%nlay)
+              call ReadArray(this%parser%iuactive, this%bot3d(:,:,:),            &
+                              aname(4), this%ndim, this%ncol, this%nrow,         &
                               this%nlay, this%iout, 1, this%nlay)
             else
               nvals = this%ncol * this%nrow * this%nlay
-              call ReadArray(this%parser%iuactive, this%botm(:,:,2:this%nlay+1), &
+              !call ReadArray(this%parser%iuactive, this%botm(:,:,2:this%nlay+1), &
+              !                aname(4), this%ndim, nvals, this%iout)
+              call ReadArray(this%parser%iuactive, this%bot3d(:,:,:),             &
                               aname(4), this%ndim, nvals, this%iout)
             end if
             lname(4) = .true.
@@ -467,11 +482,18 @@ module GwfDisModule
       do i = 1, this%nrow
         do j = 1, this%ncol
           if (this%idomain(j, i, k) < 1) cycle
-          dz = this%botm(j, i, k) - this%botm(j, i, k + 1)
+          if (k > 1) then
+            top = this%bot3d(j, i, k - 1)
+          else
+            top = this%top2d(j, i)
+          end if
+          !dz = this%botm(j, i, k) - this%botm(j, i, k + 1)
+          dz = top - this%bot3d(j, i, k)
           if (dz <= DZERO) then
             n = n + 1
-            write(ermsg, fmt=fmtdz) k, i, j, this%botm(j, i, k),                 &
-                                    this%botm(j, i, k + 1)
+            !write(ermsg, fmt=fmtdz) k, i, j, this%botm(j, i, k),                 &
+            !                        this%botm(j, i, k + 1)
+            write(ermsg, fmt=fmtdz) k, i, j, top, this%bot3d(j, i, k)
             call store_error(ermsg)
           endif
         enddo
@@ -540,8 +562,15 @@ module GwfDisModule
           noder = node
           if(this%nodes < this%nodesuser) noder = this%nodereduced(node)
           if(noder <= 0) cycle
-          this%top(noder) = this%botm(j, i, k)
-          this%bot(noder) = this%botm(j, i, k + 1)
+          !this%top(noder) = this%botm(j, i, k)
+          !this%bot(noder) = this%botm(j, i, k + 1)
+          if (k > 1) then
+            top = this%bot3d(j, i, k - 1)
+          else
+            top = this%top2d(j, i)
+          end if
+          this%top(noder) = top
+          this%bot(noder) = this%bot3d(j, i, k)
           this%area(noder) = this%delr(j) * this%delc(i)
         enddo
       enddo
@@ -675,8 +704,10 @@ module GwfDisModule
     write(iunit) this%angrot                                                    ! angrot
     write(iunit) this%delr                                                      ! delr
     write(iunit) this%delc                                                      ! delc
-    write(iunit) this%botm(:, :, 1)                                             ! top
-    write(iunit) this%botm(:, :, 2:)                                            ! botm
+    !write(iunit) this%botm(:, :, 1)                                             ! top
+    !write(iunit) this%botm(:, :, 2:)                                            ! botm
+    write(iunit) this%top2d                                                     ! top2d
+    write(iunit) this%bot3d                                                     ! bot3d
     write(iunit) this%con%iausr                                                 ! iausr
     write(iunit) this%con%jausr                                                 ! jausr
     write(iunit) this%idomain                                                   ! idomain
