93e65ac80881c370d12fad474f53f5370e491c2c
mjr-deltares <martijn.russcher@deltares.nl>
2019-10-17 10:21:12 +0100

    Fix unit tests

--- test/src/TestDis.f90
+++ test/src/TestDis.f90
@@ -20,7 +20,7 @@ contains
     class(DisBaseType), pointer :: dis   
     real(DP) :: x, y
     
-    ! create a disv with 2x6 (delr=100.0,delc=100.0) cells
+    ! create a dis with 2x6 (delr=100.0,delc=100.0) cells
     open(unit=6666, file=TESTDATADIR//"discretizations/dis_2x6.dis", status='old', action='read')
     call dis_cr(dis, 'somename4dis3d', 6666, -1)
     call dis%dis_df()
@@ -28,11 +28,11 @@ contains
     
     call dis%get_cellxy(2, x, y)
     call assert_equal_realdp(x, 150.0_DP, "x coordinate cell 2")
-    call assert_equal_realdp(y, 50.0_DP, "y coordinate cell 2")
+    call assert_equal_realdp(y, 150.0_DP, "y coordinate cell 2")
   
     call dis%get_cellxy(12, x, y)
     call assert_equal_realdp(x, 550.0_DP, "x coordinate cell 12")
-    call assert_equal_realdp(y, 150.0_DP, "y coordinate cell 12")
+    call assert_equal_realdp(y, 50.0_DP, "y coordinate cell 12")
     
   end subroutine
   
