7f87744d97c4660fe81ba0717ce40e2bae463b16
langevin-usgs <langevin@usgs.gov>
2019-10-27 09:16:30 -0500

    test(dsp): simple test to ensure diffusion works with variable layer thicknesses (#232)

---
+++ autotest/test_gwt_dsp05_noadv.py
@@ -0,0 +1,200 @@
+"""
+MODFLOW 6 Autotest
+Test variable layer thicknesses with a diffusion problem and constant
+concentrations on the top and bottom to make sure the resulting concentration
+field is linear from top to bottom.
+
+"""
+import os
+import numpy as np
+
+try:
+    import pymake
+except:
+    msg = 'Error. Pymake package is not available.\n'
+    msg += 'Try installing using the following command:\n'
+    msg += ' pip install https://github.com/modflowpy/pymake/zipball/master'
+    raise Exception(msg)
+
+try:
+    import flopy
+except:
+    msg = 'Error. FloPy package is not available.\n'
+    msg += 'Try installing using the following command:\n'
+    msg += ' pip install flopy'
+    raise Exception(msg)
+
+from framework import testing_framework
+from simulation import Simulation
+
+ex = ['dsp05a_noadv', 'dsp01b_noadv']
+xt3d = [False, True]
+exdirs = []
+for s in ex:
+    exdirs.append(os.path.join('temp', s))
+ddir = 'data'
+
+
+def get_model(idx, dir):
+    nlay, nrow, ncol = 5, 1, 1
+    nper = 1
+    perlen = [5.0]
+    nstp = [200]
+    tsmult = [1.]
+    steady = [True]
+    delr = 1.
+    delc = 1.
+    top = 2.
+    botm = [0., -.5, -1., -1.5, -2.]
+
+    nouter, ninner = 100, 300
+    hclose, rclose, relax = 1e-6, 1e-6, 1.
+
+    tdis_rc = []
+    for i in range(nper):
+        tdis_rc.append((perlen[i], nstp[i], tsmult[i]))
+
+    name = ex[idx]
+
+    # build MODFLOW 6 files
+    ws = dir
+    sim = flopy.mf6.MFSimulation(sim_name=name, version='mf6',
+                                 exe_name='mf6',
+                                 sim_ws=ws)
+    # create tdis package
+    tdis = flopy.mf6.ModflowTdis(sim, time_units='DAYS',
+                                 nper=nper, perioddata=tdis_rc)
+
+    # create gwt model
+    gwtname = 'gwt_' + name
+    gwt = flopy.mf6.MFModel(sim, model_type='gwt6', modelname=gwtname,
+                            model_nam_file='{}.nam'.format(gwtname))
+    gwt.name_file.save_flows = True
+
+    # create iterative model solution and register the gwt model with it
+    imsgwt = flopy.mf6.ModflowIms(sim, print_option='SUMMARY',
+                                  outer_hclose=hclose,
+                                  outer_maximum=nouter,
+                                  under_relaxation='NONE',
+                                  inner_maximum=ninner,
+                                  inner_hclose=hclose, rcloserecord=rclose,
+                                  linear_acceleration='BICGSTAB',
+                                  scaling_method='NONE',
+                                  reordering_method='NONE',
+                                  relaxation_factor=relax,
+                                  filename='{}.ims'.format(gwtname))
+    sim.register_ims_package(imsgwt, [gwt.name])
+
+    dis = flopy.mf6.ModflowGwtdis(gwt, nlay=nlay, nrow=nrow, ncol=ncol,
+                                  delr=delr, delc=delc,
+                                  top=top, botm=botm,
+                                  idomain=1,
+                                  filename='{}.dis'.format(gwtname))
+
+    # initial conditions
+    ic = flopy.mf6.ModflowGwtic(gwt, strt=0.,
+                                filename='{}.ic'.format(gwtname))
+
+    # dispersion
+    dsp = flopy.mf6.ModflowGwtdsp(gwt, xt3d=xt3d[idx], diffc=100.,
+                                  alh=0., alv=0., ath1=0., atv=0.,
+                                  filename='{}.dsp'.format(gwtname))
+
+    # constant concentration
+    cncs = {0: [[(0, 0, 0), 1.0], [(nlay - 1, 0, 0), 0.0]]}
+    cnc = flopy.mf6.ModflowGwtcnc(gwt, maxbound=len(cncs),
+                                  stress_period_data=cncs,
+                                  save_flows=False,
+                                  pname='CNC-1')
+
+    # mass storage and transfer
+    mst = flopy.mf6.ModflowGwtmst(gwt, porosity=0.1)
+
+    # output control
+    oc = flopy.mf6.ModflowGwtoc(gwt,
+                                budget_filerecord='{}.cbc'.format(gwtname),
+                                concentration_filerecord='{}.ucn'.format(gwtname),
+                                concentrationprintrecord=[
+                                    ('COLUMNS', 10, 'WIDTH', 15,
+                                     'DIGITS', 6, 'GENERAL')],
+                                saverecord=[('CONCENTRATION', 'LAST'),
+                                            ('BUDGET', 'LAST')],
+                                printrecord=[('CONCENTRATION', 'LAST'),
+                                             ('BUDGET', 'LAST')])
+    print(gwt.modelgrid.zcellcenters)
+    return sim
+
+
+def build_models():
+    for idx, dir in enumerate(exdirs):
+        sim = get_model(idx, dir)
+        sim.write_simulation()
+    return
+
+
+def eval_transport(sim):
+    print('evaluating transport...')
+
+    name = ex[sim.idxsim]
+    gwtname = 'gwt_' + name
+
+    fpth = os.path.join(sim.simpath, '{}.ucn'.format(gwtname))
+    try:
+        cobj = flopy.utils.HeadFile(fpth, precision='double',
+                                    text='CONCENTRATION')
+        conc = cobj.get_data()
+    except:
+        assert False, 'could not load data from "{}"'.format(fpth)
+
+    # The answer is a linear concentration gradient from 1 to zero
+    x = np.array([1., -0.25, -0.75, -1.25, -1.75])
+    x0 = x[0]
+    xe = x[-1]
+    c0 = 1.
+    ce = 0.
+    m = (c0 - ce) / (x0 - xe)
+    b = c0 - m * x0
+    cres = m * x + b
+    msg = ('simulated concentrations do not match with known '
+           'solution. {} {}'.format(conc, cres))
+    assert np.allclose(cres, conc.flatten()), msg
+
+    return
+
+
+# - No need to change any code below
+def test_mf6model():
+    # initialize testing framework
+    test = testing_framework()
+
+    # build the models
+    build_models()
+
+    # run the test models
+    for idx, dir in enumerate(exdirs):
+        yield test.run_mf6, Simulation(dir, exfunc=eval_transport, idxsim=idx)
+
+    return
+
+
+def main():
+    # initialize testing framework
+    test = testing_framework()
+
+    # build the models
+    build_models()
+
+    # run the test models
+    for idx, dir in enumerate(exdirs):
+        sim = Simulation(dir, exfunc=eval_transport, idxsim=idx)
+        test.run_mf6(sim)
+
+    return
+
+
+if __name__ == "__main__":
+    # print message
+    print('standalone run of {}'.format(os.path.basename(__file__)))
+
+    # run main routine
+    main()
