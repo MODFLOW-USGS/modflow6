7711b2cb91eec568159ac7c9bcbb4c9fb1fb2322
jdhughes-usgs <jdhughes@usgs.gov>
2020-04-30 17:28:13 -0400

    ci(update): update to travis yml - patterned after flopy

---
+++ travis/before_script.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+set -e
+
+
+export BRANCH=$(if [ "$TRAVIS_PULL_REQUEST" == "false" ]; then echo $TRAVIS_BRANCH;
+  else echo $TRAVIS_PULL_REQUEST_BRANCH; fi)
+echo "TRAVIS_BRANCH=$TRAVIS_BRANCH, PR=$PR, BRANCH=$BRANCH"
+pwd
+git clone https://github.com/MODFLOW-USGS/modflow6-examples ../modflow6-examples
+cd ../modflow6-examples
+if git show-ref -q --heads $BRANCH; then
+  git checkout $BRANCH; echo switched to modflow6-examples branch $BRANCH;
+  else echo using modflow6-examples branch master;  fi
+git branch
+cd ../modflow6
+ls ../
+which python
+python --version
+python -c "import numpy as np; print('numpy version {}'.format(np.__version__))"
+python -c "import flopy; flopypth = flopy.__path__[0]; print('flopy is installed
+  in {}'.format(flopypth))"
+python -c "import flopy; dir(flopy.mf6)"
+cd ./autotest
+python update_flopy.py
+cd ..
+python -c "import flopy; dir(flopy.mf6)"
+which nosetests
+nosetests --version
