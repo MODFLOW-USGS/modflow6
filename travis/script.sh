4cdf7eaff5939a20f07eaa66af1d554de8167697
jdhughes-usgs <jdhughes@usgs.gov>
2020-05-01 11:15:43 -0400

    ci(update): rework travis.yml

--- travis/script.sh
+++ travis/script.sh
@@ -5,7 +5,7 @@ set -e
 if [ "${FC}" = "gfortran-8" ]; then
   echo "building mfio latex document"
   cd autotest/
-  nosetests -v --with-id --with-timer -w./autotest -m build_mfio_tex.py
+  nosetests -v --with-id --with-timer build_mfio_tex.py
   cd ../
 fi
 nosetests -v --with-id --with-timer -w ./autotest
