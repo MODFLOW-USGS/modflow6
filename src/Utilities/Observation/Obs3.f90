f3533c6440bc3f6ae2bdd5d613acb60fb90af98e
unknown <jwhiteusgs@gmail.com>
2020-07-20 10:29:43 -0600

    handling case where multiple blocks are defined in the same obs file

--- src/Utilities/Observation/Obs3.f90
+++ src/Utilities/Observation/Obs3.f90
@@ -827,7 +827,7 @@ contains
     ! -- dummy
     class(ObsType), target :: this
     ! -- local
-    integer(I4B) :: i, ii, idx, indx, iu, num, nunit,iidx
+    integer(I4B) :: i, ii, idx, indx, iu, num, nunit,oidx,last_nunit
     integer(int32) :: nobs
     character(len=LENOBSNAME), pointer :: headr => null()
     character(len=LENOBSNAME)          :: nam
@@ -839,14 +839,13 @@ contains
     num = this%obsList%Count()
     ! -- Cycle through observations to build the header(s)
     if (num>0) then
-        ! jwhite: get the current size of the obsnames ar        
-        obsrv => this%get_obs(1)
-        ! -- header for file of continuous observations
-        indx = obsrv%indxObsOutput
-        obsOutput => this%obsOutputList%Get(indx)
-        iidx = size(obsOutput%obsnames)
-        ! jwhite: now expand the obsnames array by num
-        call ExpandArray(obsOutput%obsnames,num)
+      ! reserve the space for the first obs output file  
+      obsrv => this%get_obs(1) 
+      indx = obsrv%indxObsOutput
+      obsOutput => this%obsOutputList%Get(indx)
+      last_nunit = obsOutput%NUNIT ! track the nunit as the key for when to reserve new output space
+      call ExpandArray(obsOutput%obsnames,obsOutput%NOBS)
+      oidx = 1
       do i=1,num
         obsrv => this%get_obs(i)
         ! -- header for file of continuous observations
@@ -856,11 +855,15 @@ contains
         if (headr == '') then
           headr = 'time'
         endif
+        ! if the current nunit is new, reserve some more
+        if (obsOutput%NUNIT.ne.last_nunit) then
+          call ExpandArray(obsOutput%obsnames,obsOutput%NOBS)
+          last_nunit = obsOutput%NUNIT
+          oidx = 1
+        endif
         nam = obsrv%Name
-        !call ExpandArray(obsOutput%obsnames)
-        !idx = size(obsOutput%obsnames)
-        idx = iidx + i
-        obsOutput%obsnames(idx) = nam
+        obsOutput%obsnames(oidx) = nam
+        oidx = oidx + 1
       enddo
     endif
     !
