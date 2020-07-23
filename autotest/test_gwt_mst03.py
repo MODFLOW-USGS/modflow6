20f2054b4f878144666bc7ff789834a67258855e
Christian Langevin <langevin@usgs.gov>
2020-07-23 15:56:43 -0500

    tests(mst): add test for GWT MST Package

---
+++ autotest/test_gwt_mst03.py
@@ -0,0 +1,310 @@
+"""
+Test the MST Package and its ability to calculate simple mixing for a one-cell
+model.  Patterned after the simple test presented in the MT3D-USGS manual on
+pages 9-10.
+
+"""
+
+import os
+import numpy as np
+
+try:
+    import pymake
+except:
+    msg = 'Error. Pymake package is not available.\n'
+    msg += 'Try installing using the following command:\n'
+    msg += ' pip install https://github.com/modflowpy/pymake/zipball/master'
+    raise Exception(msg)
+
+try:
+    import flopy
+except:
+    msg = 'Error. FloPy package is not available.\n'
+    msg += 'Try installing using the following command:\n'
+    msg += ' pip install flopy'
+    raise Exception(msg)
+
+from framework import testing_framework
+from simulation import Simulation
+
+ex = ['mst03']
+laytyp = [1]
+ss = [1.e-10]
+sy = [0.1]
+exdirs = []
+for s in ex:
+    exdirs.append(os.path.join('temp', s))
+ddir = 'data'
+nlay, nrow, ncol = 1, 1, 1
+
+
+def build_models():
+
+    nper = 2
+    perlen = [2., 2.]
+    nstp = [14, 14]
+    tsmult = [1., 1.]
+    delr = 10.
+    delc = 10.
+    top = 10.
+    botm = [0.]
+    strt = top
+    hk = 1.0
+
+    nouter, ninner = 100, 300
+    hclose, rclose, relax = 1e-6, 1e-6, 0.97
+
+    tdis_rc = []
+    for idx in range(nper):
+        tdis_rc.append((perlen[idx], nstp[idx], tsmult[idx]))
+
+    for idx, dir in enumerate(exdirs):
+        name = ex[idx]
+
+        # build MODFLOW 6 files
+        ws = dir
+        sim = flopy.mf6.MFSimulation(sim_name=name, version='mf6',
+                                     exe_name='mf6',
+                                     sim_ws=ws)
+        # create tdis package
+        tdis = flopy.mf6.ModflowTdis(sim, time_units='DAYS',
+                                     nper=nper, perioddata=tdis_rc)
+
+        # create gwf model
+        gwfname = 'gwf_' + name
+        newtonoptions = ['NEWTON', 'UNDER_RELAXATION']
+        gwf = flopy.mf6.ModflowGwf(sim, modelname=gwfname,
+                                   newtonoptions=newtonoptions,)
+
+        # create iterative model solution and register the gwf model with it
+        imsgwf = flopy.mf6.ModflowIms(sim, print_option='SUMMARY',
+                                      outer_dvclose=hclose,
+                                      outer_maximum=nouter,
+                                      under_relaxation='DBD',
+                                      under_relaxation_theta=0.7,
+                                      inner_maximum=ninner,
+                                      inner_dvclose=hclose, rcloserecord=rclose,
+                                      linear_acceleration='BICGSTAB',
+                                      scaling_method='NONE',
+                                      reordering_method='NONE',
+                                      relaxation_factor=relax,
+                                      filename='{}.ims'.format(gwfname))
+        sim.register_ims_package(imsgwf, [gwf.name])
+
+        dis = flopy.mf6.ModflowGwfdis(gwf, nlay=nlay, nrow=nrow, ncol=ncol,
+                                      delr=delr, delc=delc,
+                                      top=top, botm=botm,
+                                      idomain=np.ones((nlay, nrow, ncol),
+                                                      dtype=np.int))
+
+        # initial conditions
+        ic = flopy.mf6.ModflowGwfic(gwf, strt=strt)
+
+        # node property flow
+        npf = flopy.mf6.ModflowGwfnpf(gwf, save_flows=False,
+                                      icelltype=laytyp[idx],
+                                      k=hk, k33=hk)
+        # storage
+        sto = flopy.mf6.ModflowGwfsto(gwf, save_flows=False,
+                                      iconvert=laytyp[idx],
+                                      ss=ss[idx], sy=sy[idx],
+                                      steady_state={0: False},
+                                      transient={0: True})
+
+        # wel files
+        welspdict = {0: [[(0, 0, 0), -25., 0.]],
+                     1: [[(0, 0, 0), 25., 0.]]}
+        wel = flopy.mf6.ModflowGwfwel(gwf, print_input=True, print_flows=True,
+                                      stress_period_data=welspdict,
+                                      save_flows=False,
+                                      auxiliary='CONCENTRATION', pname='WEL-1')
+
+        # output control
+        oc = flopy.mf6.ModflowGwfoc(gwf,
+                                    budget_filerecord='{}.cbc'.format(gwfname),
+                                    head_filerecord='{}.hds'.format(gwfname),
+                                    headprintrecord=[
+                                        ('COLUMNS', 10, 'WIDTH', 15,
+                                         'DIGITS', 6, 'GENERAL')],
+                                    saverecord=[('HEAD', 'ALL')],
+                                    printrecord=[('HEAD', 'ALL'),
+                                                 ('BUDGET', 'ALL')])
+
+        # create gwt model
+        gwtname = 'gwt_' + name
+        gwt = flopy.mf6.MFModel(sim, model_type='gwt6', modelname=gwtname,
+                                model_nam_file='{}.nam'.format(gwtname))
+
+        # create iterative model solution and register the gwt model with it
+        imsgwt = flopy.mf6.ModflowIms(sim, print_option='SUMMARY',
+                                      outer_dvclose=hclose,
+                                      outer_maximum=nouter,
+                                      under_relaxation='NONE',
+                                      inner_maximum=ninner,
+                                      inner_dvclose=hclose, rcloserecord=rclose,
+                                      linear_acceleration='BICGSTAB',
+                                      scaling_method='NONE',
+                                      reordering_method='NONE',
+                                      relaxation_factor=relax,
+                                      filename='{}.ims'.format(gwtname))
+        sim.register_ims_package(imsgwt, [gwt.name])
+
+        dis = flopy.mf6.ModflowGwtdis(gwt, nlay=nlay, nrow=nrow, ncol=ncol,
+                                      delr=delr, delc=delc,
+                                      top=top, botm=botm,
+                                      idomain=1,
+                                      filename='{}.dis'.format(gwtname))
+
+        # initial conditions
+        ic = flopy.mf6.ModflowGwtic(gwt, strt=100.)
+
+        # advection
+        adv = flopy.mf6.ModflowGwtadv(gwt, scheme='UPSTREAM',
+                                    filename='{}.adv'.format(gwtname))
+
+        # mass storage and transfer
+        mst = flopy.mf6.ModflowGwtmst(gwt, porosity=sy[idx],
+                                    filename='{}.mst'.format(gwtname))
+
+        # sources
+        sourcerecarray = [('WEL-1', 'AUX', 'CONCENTRATION')]
+        ssm = flopy.mf6.ModflowGwtssm(gwt, sources=sourcerecarray,
+                                    filename='{}.ssm'.format(gwtname))
+
+        # output control
+        oc = flopy.mf6.ModflowGwtoc(gwt,
+                                    budget_filerecord='{}.cbc'.format(gwtname),
+                                    concentration_filerecord='{}.ucn'.format(gwtname),
+                                    concentrationprintrecord=[
+                                        ('COLUMNS', 10, 'WIDTH', 15,
+                                         'DIGITS', 6, 'GENERAL')],
+                                    saverecord=[('CONCENTRATION', 'ALL')],
+                                    printrecord=[('CONCENTRATION', 'ALL'),
+                                                 ('BUDGET', 'ALL')])
+
+        # GWF GWT exchange
+        gwfgwt = flopy.mf6.ModflowGwfgwt(sim, exgtype='GWF6-GWT6',
+                                         exgmnamea=gwfname, exgmnameb=gwtname,
+                                         filename='{}.gwfgwt'.format(name))
+
+        # write MODFLOW 6 files
+        sim.write_simulation()
+
+    return
+
+
+def eval_transport(sim):
+    print('evaluating transport...')
+
+    name = ex[sim.idxsim]
+    gwtname = 'gwt_' + name
+    gwfname = 'gwf_' + name
+
+    fpth = os.path.join(sim.simpath, '{}.hds'.format(gwfname))
+    try:
+        hobj = flopy.utils.HeadFile(fpth, precision='double')
+        head = hobj.get_alldata().flatten()
+    except:
+        assert False, 'could not load data from "{}"'.format(fpth)
+
+    fpth = os.path.join(sim.simpath, '{}.ucn'.format(gwtname))
+    try:
+        cobj = flopy.utils.HeadFile(fpth, precision='double',
+                                    text='CONCENTRATION')
+        conc = cobj.get_alldata().flatten()
+    except:
+        assert False, 'could not load data from "{}"'.format(fpth)
+
+    # calculations
+    times = hobj.get_times()
+    print('times', times)
+    nstp = len(times)
+    print('nstp', nstp)
+    dt = 4. / nstp
+    print('dt', dt)
+    volume_calc = []
+    head_calc = []
+    concentration_calc = []
+    vold = 1000 * .1
+    mass_old = vold * 100.
+    csrc = 0.
+    cold = mass_old / vold
+    for t in times:
+        q = -25.
+        if t > 2.:
+            q = 25.
+        v = vold + q * dt
+        vold = v
+        volume_calc.append(v)
+        h = v / 0.1 / 10. / 10.
+        head_calc.append(h)
+
+        csrc = cold
+        if t > 2.:
+            csrc = 0.
+        mass_stored = mass_old + q * csrc * dt
+        mass_old = mass_stored
+        concentration = mass_stored / v
+        concentration_calc.append(concentration)
+        cold = concentration
+
+    # compare calculated and simulated volume
+    volume_sim = head * 10 * 1.
+    print('volume sim', volume_sim)
+    print('volume calc', volume_calc)
+    errmsg = '{}\n{}'.format(volume_calc, volume_sim, atol=1.e-8)
+    assert np.allclose(volume_calc, volume_sim), errmsg
+
+    # compare calculated and simulated head
+    hanswer = np.array(head_calc)
+    print('head sim', head)
+    print('head calc', hanswer)
+    errmsg = '{}\n{}'.format(head, hanswer, atol=1.e-8)
+    assert np.allclose(head, hanswer), errmsg
+
+    # compare calculated and simulated concentration
+    print('concentration sim', conc)
+    print('concentration calc', concentration_calc)
+    canswer = np.array(concentration_calc)
+    errmsg = '{}\n{}'.format(conc, canswer)
+    assert np.allclose(conc, canswer, atol=1.e-8), errmsg
+
+    return
+
+
+# - No need to change any code below
+def test_mf6model():
+    # initialize testing framework
+    test = testing_framework()
+
+    # build the models
+    build_models()
+
+    # run the test models
+    for idx, dir in enumerate(exdirs):
+        yield test.run_mf6, Simulation(dir, exfunc=eval_transport, idxsim=idx)
+
+    return
+
+
+def main():
+    # initialize testing framework
+    test = testing_framework()
+
+    # build the models
+    build_models()
+
+    # run the test models
+    for idx, dir in enumerate(exdirs):
+        sim = Simulation(dir, exfunc=eval_transport, idxsim=idx)
+        test.run_mf6(sim)
+
+    return
+
+
+if __name__ == "__main__":
+    # print message
+    print('standalone run of {}'.format(os.path.basename(__file__)))
+
+    # run main routine
+    main()
