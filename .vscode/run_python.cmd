0625f0b4905029d00a93b425d1f34eeb26d57344
mjr-deltares <martijn.russcher@deltares.nl>
2020-08-11 13:09:23 +0100

    small fix: made templated path in script anonymous

--- .vscode/run_python.cmd
+++ .vscode/run_python.cmd
@@ -3,7 +3,7 @@ rem there
 cd %~dp0\..\pymake
 
 rem activate "your_conda_env" for building the source code with pymake
-call C:\Users\russcher\AppData\Local\Continuum\anaconda3\condabin\conda.bat activate your_conda_env
+call /path/to/your/conda.bat activate your_conda_env
 
 rem build with pymake
 python %1 %2 %3 %4 %5
