461e4a863552bed98e8d97648feb653c31489860
jdhughes-usgs <jdhughes@usgs.gov>
2020-08-17 17:51:27 -0400

    fix(maw): fix bug in maw newton MAW-GWF terms (#549)
    Add additional error checking of maw input variables. Added
    clarification of when and how user-specified values are reset for
    the various conductance equations. Add additional test to evaluate
    Newton-Raphson solution for a steady-state and transient drying
    multi-aquifer well problem (test_gwf_maw08.py and test_gwf_maw09.py,
    respectively).
    
    Closes #546

--- autotest/test_gwf_maw08.py
+++ autotest/test_gwf_maw08.py
@@ -1,6 +1,5 @@
-# Modifiy the previous test by having a first stress period where the
-# MAW well is inactive.  Test ensures that gwf-maw and maw-gwf flows reported
-# in the gwf and maw budget files are zero for this first period.
+# test to evaluate Newton-Raphson solution for a single column steady-state
+# dry multi-aquifer well problem. Developed to address issue #546
 
 import os
 import numpy as np
@@ -16,51 +15,43 @@ except:
 from framework import testing_framework
 from simulation import Simulation
 
-ex = ['maw_07a', 'maw_07b']
+ex = ("maw_08a", "maw_08b")
 exdirs = []
 for s in ex:
     exdirs.append(os.path.join('temp', s))
+dis_option = ("dis", "disv")
 
-nlay = 2
+nlay = 3
 nrow = 1
 ncol = 1
+
+vertices = [
+            (0, 0, 0),
+            (1, 0, 1),
+            (2, 1, 1),
+            (3, 1, 0),
+            ]
+cell2d = [
+          (0, 0.5, 0.5, 5, 0, 1, 2, 3, 0),
+         ]
 
 delc = 1.
 delr = 1.
 gwfarea = delr * delc
 
-top = 2.
-bot = 0.
-aqthick = top - bot
-dz = aqthick / float(nlay)
-botm = [top - dz * (k + 1) for k in range(nlay)]
-ztop = [top - dz * k for k in range(nlay)]
+top = 30.
+botm = [20, 10, 0]
+maw_bot = 15.
 
-strt_min = aqthick / 8.
-mawstrt = [strt_min, top,]
-gwfstrt = [top, strt_min,]
+strt = 4.5
+maw_strt = 16.
 
-Kh = 1.
+Kh = 10
 Kv = 1.
-sy = 1.
-ss = 0.
-
-mawarea = 1.
-mawradius = np.sqrt(mawarea / np.pi) #.65
-mawcond = Kh * delc * dz / (0.5 * delr)
+radius = 0.05
 
 def get_model(idx, dir):
-    nper = 2
-    perlen = [10.0, 10.0]
-    nstp = [1, 100]
-    tsmult = [1, 1.005]
-
-    tdis_rc = []
-    for i in range(nper):
-        tdis_rc.append((perlen[i], nstp[i], tsmult[i]))
-
-    nouter, ninner = 700, 200
-    hclose, rclose, relax = 1e-9, 1e-9, 1.0
+    dvclose, rclose, relax = 1e-9, 1e-9, 1.0
 
     name = ex[idx]
 
@@ -72,59 +63,70 @@ def get_model(idx, dir):
                                  sim_ws=ws,
                                  memory_print_option='summary')
     # create tdis package
-    tdis = flopy.mf6.ModflowTdis(sim, time_units='DAYS',
-                                 nper=nper, perioddata=tdis_rc)
+    tdis = flopy.mf6.ModflowTdis(sim, time_units='DAYS')
+
+    imsgwf = flopy.mf6.ModflowIms(sim, print_option='SUMMARY',
+                                  complexity="complex",
+                                  outer_dvclose=dvclose,
+                                  inner_dvclose=dvclose,
+                                  rcloserecord=[rclose, 'strict'],
+                                  relaxation_factor=relax,
+                                  )
 
     # create gwf model
     gwfname = 'gwf_' + name
 
     newtonoptions = ['NEWTON', 'UNDER_RELAXATION']
     gwf = flopy.mf6.ModflowGwf(sim, modelname=gwfname,
-                               newtonoptions=newtonoptions)
+                               newtonoptions=newtonoptions, print_input=True)
 
-    imsgwf = flopy.mf6.ModflowIms(sim, print_option='SUMMARY',
-                                  outer_hclose=hclose,
-                                  outer_maximum=nouter,
-                                  under_relaxation='SIMPLE',
-                                  under_relaxation_gamma=0.98,
-                                  inner_maximum=ninner,
-                                  inner_hclose=hclose,
-                                  rcloserecord=[rclose, 'strict'],
-                                  linear_acceleration='BICGSTAB',
-                                  scaling_method='NONE',
-                                  reordering_method='NONE',
-                                  relaxation_factor=relax,
-                                  filename='{}.ims'.format(gwfname))
+    if dis_option[idx] == "dis":
+        dis = flopy.mf6.ModflowGwfdis(gwf, nlay=nlay, nrow=nrow, ncol=ncol,
+                                      delr=delr, delc=delc,
+                                      top=top, botm=botm)
+        cellids = [
+                   (0, 0, 0),
+                   (1, 0, 0),
+                  ]
+    else:
+        dis = flopy.mf6.ModflowGwfdisv(gwf,
+                                       nlay=nlay, ncpl=nrow, nvert=4,
+                                       vertices=vertices, cell2d=cell2d,
+                                       top=top, botm=botm)
+        cellids = [
+                   (0, 0),
+                   (1, 0),
+                  ]
 
-    dis = flopy.mf6.ModflowGwfdis(gwf, nlay=nlay, nrow=nrow, ncol=ncol,
-                                  delr=delr, delc=delc,
-                                  top=top, botm=botm)
 
     # initial conditions
-    ic = flopy.mf6.ModflowGwfic(gwf, strt=gwfstrt[idx])
+    ic = flopy.mf6.ModflowGwfic(gwf, strt=strt)
 
     # node property flow
-    npf = flopy.mf6.ModflowGwfnpf(gwf, xt3doptions=False,
+    npf = flopy.mf6.ModflowGwfnpf(gwf,
                                   save_flows=True,
                                   save_specific_discharge=True,
                                   icelltype=1,
                                   k=Kh, k33=Kv)
 
-    sto = flopy.mf6.ModflowGwfsto(gwf, sy=sy, ss=ss, iconvert=1)
-
-    mstrt = mawstrt[idx]
-    mawcondeqn = 'SPECIFIED'
-    mawngwfnodes = nlay
     # <wellno> <radius> <bottom> <strt> <condeqn> <ngwfnodes>
-    mawpackagedata = [
-        [0, mawradius, bot, mstrt, mawcondeqn, mawngwfnodes]]
+    mawpackagedata = flopy.mf6.ModflowGwfmaw.packagedata.empty(gwf, maxbound=1)
+    mawpackagedata["radius"] = radius
+    mawpackagedata["bottom"] = maw_bot
+    mawpackagedata["strt"] = maw_strt
+    mawpackagedata["condeqn"] = "thiem"
+    mawpackagedata["ngwfnodes"] = 2
+
     # <wellno> <icon> <cellid(ncelldim)> <scrn_top> <scrn_bot> <hk_skin> <radius_skin>
-    mawconnectiondata = [[0, icon, (icon, 0, 0), top, bot, mawcond, -999]
-                         for icon in range(nlay)]
-    # <wellno> <mawsetting>
-    mawperioddata = {}
-    mawperioddata[0] = [[0, 'STATUS', 'INACTIVE']]
-    mawperioddata[1] = [[0, 'STATUS', 'ACTIVE']]
+    mawconnectiondata = flopy.mf6.ModflowGwfmaw.connectiondata.empty(gwf,
+                                                                     maxbound=2)
+    mawconnectiondata["icon"] = [0, 1]
+    mawconnectiondata["cellid"] = cellids
+    mawconnectiondata["scrn_top"] = 100.
+    mawconnectiondata["scrn_bot"] = 0.
+    mawconnectiondata["hk_skin"] = -999
+    mawconnectiondata["radius_skin"] = -999
+
     mbin = '{}.maw.bin'.format(gwfname)
     mbud = '{}.maw.bud'.format(gwfname)
     maw = flopy.mf6.ModflowGwfmaw(gwf,
@@ -132,13 +134,10 @@ def get_model(idx, dir):
                                   print_head=True,
                                   print_flows=True,
                                   save_flows=True,
-                                  flow_correction=True,
                                   head_filerecord=mbin,
                                   budget_filerecord=mbud,
                                   packagedata=mawpackagedata,
                                   connectiondata=mawconnectiondata,
-                                  perioddata=mawperioddata,
-                                  pname='MAW-1',
                                   )
     opth = '{}.maw.obs'.format(gwfname)
     obsdata = {'{}.maw.obs.csv'.format(gwfname): [('whead', 'head', (0,)), ]}
@@ -179,47 +178,16 @@ def eval_results(sim):
     fname = os.path.join(sim.simpath, fname)
     assert os.path.isfile(fname)
     bobj = flopy.utils.HeadFile(fname, text='HEAD')
-    stage = bobj.get_alldata().flatten()[1:]
+
+    well_head = bobj.get_data().flatten()
+    assert np.allclose(well_head, 10.), \
+        "simulated maw head ({}) does not equal 10.".format(well_head[0])
 
     fname = gwfname + '.hds'
     fname = os.path.join(sim.simpath, fname)
     assert os.path.isfile(fname)
     hobj = flopy.utils.HeadFile(fname)
     head = hobj.get_alldata()[1:]
-
-    # calculate initial volume of water in well and aquifer
-    v0maw = mawstrt[sim.idxsim] * mawarea
-    v0gwf = (gwfstrt[sim.idxsim] - bot) * sy * gwfarea
-    v0 = v0maw + v0gwf
-
-    print('Initial volumes\n' +
-          '  Groundwater:    {}\n'.format(v0gwf) +
-          '  Well:           {}\n'.format(v0maw) +
-          '  Total:          {}'.format(v0))
-
-    # calculate current volume of water in well and aquifer and compare with
-    # initial volume
-    for kstp, mawstage in enumerate(stage):
-        vgwf = 0.
-        for k in range(nlay):
-            for j in range(ncol):
-                tp = min(head[kstp, k, 0, j], ztop[k])
-                dz = tp - botm[k]
-                vgwf += max(0., dz) * sy * gwfarea
-        vmaw = (stage[kstp] - bot) * mawarea
-        vnow = vmaw + vgwf
-        errmsg = 'kstp {}: \n'.format(kstp+1) + \
-                 '  Groundwater:   {}\n'.format(vgwf) + \
-                 '  Well:          {}\n'.format(vmaw) + \
-                 '  Total:         {}\n'.format(vnow) + \
-                 '  Initial Total: {}'.format(v0)
-        assert np.allclose(v0, vnow), errmsg
-
-    print('kstp {}: \n'.format(kstp + 1) + \
-          '  Groundwater:   {}\n'.format(vgwf) + \
-          '  Well:          {}\n'.format(vmaw) + \
-          '  Total:         {}\n'.format(vnow) + \
-          '  Initial Total: {}'.format(v0))
 
     # compare the maw-gwf flows with the gwf-maw flows
     fname = gwfname + '.maw.bud'
@@ -240,8 +208,6 @@ def eval_results(sim):
         for i in range(ra_maw.shape[0]):
             qmaw = ra_maw[i]['q']
             qgwf = ra_gwf[i]['q']
-            if istp == 0:
-                assert np.allclose(qmaw, 0.), 'inactive well, flow should be 0.'
             msg = 'step {} record {} comparing qmaw with qgwf: {} {}'.format(istp, i, qmaw, qgwf)
             print(msg)
             assert np.allclose(qmaw, -qgwf), msg
