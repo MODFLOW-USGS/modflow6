d711450334fc4dd83dbc56668ffdb62fa500cbd1
Christian Langevin <langevin@usgs.gov>
2020-10-22 12:19:36 -0500

    release(6.2.0): final changes for release

--- doc/version.py
+++ doc/version.py
@@ -1,7 +1,7 @@
 # MODFLOW 6 version file automatically created using...make_release.py
-# created on...September 02, 2020 16:52:10
+# created on...October 22, 2020 08:43:28
 
 major = 6
-minor = 1
-micro = 2
+minor = 2
+micro = 0
 __version__ = '{:d}.{:d}.{:d}'.format(major, minor, micro)
--- src/Utilities/version.f90
+++ src/Utilities/version.f90
@@ -3,8 +3,8 @@ module VersionModule
   implicit none
   public
   ! -- modflow 6 version
-  integer(I4B), parameter :: IDEVELOPMODE = 1
-  character(len=40), parameter :: VERSION = '6.1.2 06/15/2020'
+  integer(I4B), parameter :: IDEVELOPMODE = 0
+  character(len=40), parameter :: VERSION = '6.2.0 10/22/2020'
   character(len=10), parameter :: MFVNAM = ' 6'
   character(len=*), parameter  :: MFTITLE =                                     &
     'U.S. GEOLOGICAL SURVEY MODULAR HYDROLOGIC MODEL'
@@ -15,17 +15,19 @@ module VersionModule
   ! -- disclaimer must be appropriate for version (release or release candidate)
   character(len=*), parameter :: FMTDISCLAIMER =                                &
     "(/,                                                                        &
-    &'This software is preliminary or provisional and is subject to ',/,        &
-    &'revision. It is being provided to meet the need for timely best ',/,      &
-    &'science. The software has not received final approval by the U.S. ',/,    &
-    &'Geological Survey (USGS). No warranty, expressed or implied, is made ',/, &
-    &'by the USGS or the U.S. Government as to the functionality of the ',/,    &
-    &'software and related material nor shall the fact of release ',/,          &
-    &'constitute any such warranty. The software is provided on the ',/,        &
-    &'condition that neither the USGS nor the U.S. Government shall be held ',/,&
-    &'liable for any damages resulting from the authorized or unauthorized ',/, &
-    &'use of the software.',/)"
-  
+    &'This software has been approved for release by the U.S. Geological ',/,   &
+    &'Survey (USGS). Although the software has been subjected to rigorous ',/,  &
+    &'review, the USGS reserves the right to update the software as needed ',/, &
+    &'pursuant to further analysis and review. No warranty, expressed or ',/,   &
+    &'implied, is made by the USGS or the U.S. Government as to the ',/,        &
+    &'functionality of the software and related material nor shall the ',/,     &
+    &'fact of release constitute any such warranty. Furthermore, the ',/,       &
+    &'software is released on condition that neither the USGS nor the U.S. ',/, &
+    &'Government shall be held liable for any damages resulting from its ',/,   &
+    &'authorized or unauthorized use. Also refer to the USGS Water ',/,         &
+    &'Resources Software User Rights Notice for complete use, copyright, ',/,   &
+    &'and distribution information.',/)"
+
   contains
 
   subroutine write_listfile_header(iout, cmodel_type, write_sys_command, &
@@ -99,6 +101,6 @@ module VersionModule
     ! -- return
     return
   end subroutine write_listfile_header
-  
+
 end module VersionModule
 
