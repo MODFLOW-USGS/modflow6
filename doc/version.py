0159d259d804693f5a78e7c55d7be4ae80ee10e0
Christian Langevin <langevin@usgs.gov>
2020-10-22 12:25:14 -0500

    ci(release): make a fresh start on develop marked as 6.2.1

--- doc/version.py
+++ doc/version.py
@@ -1,7 +1,7 @@
 # MODFLOW 6 version file automatically created using...make_release.py
-# created on...October 22, 2020 08:43:28
+# created on...October 22, 2020 12:23:55
 
 major = 6
 minor = 2
-micro = 0
+micro = 1
 __version__ = '{:d}.{:d}.{:d}'.format(major, minor, micro)
