774e89b4df44f2f8da805e7d3b2db54f3a740ae0
langevin-usgs <langevin@usgs.gov>
2021-02-09 07:17:33 -0600

    refactor(vscode): setup vscode support for mac/linux and windows (#632)
    * refactor(vscode): setup vscode support for mac/linux and windows
    
    * use os.path.join

--- .vscode/build_vscode.py
+++ .vscode/build_vscode.py
@@ -3,50 +3,60 @@ import sys
 
 import pymake
 
+dll_extension = '.so'
+exe_extension = ''
+if sys.platform.lower() == "win32":
+    dll_extension = '.dll'
+    exe_extension = '.exe'
+
+
 def build_mf6(mode, onlytouched):
     
+    target = os.path.join('..', 'bin', 'mf6')
     fc = 'gfortran'
     cc = 'gcc'
-    
     srcdir = os.path.join('..', 'src')
+
     if mode =="-release":
       # release mode options:
-      target = '../bin/mf6.exe'    
       fflags = ('-Wtabs -Wline-truncation -Wunused-label '
                 '-Wunused-variable -pedantic -std=f2008')
     else:
-      # debug:
-      target = '../bin/mf6d.exe'  
+      # debug mode options:
+      target += 'd'
       fflags = ('-Wtabs -Wline-truncation -Wunused-label '
                 '-Wunused-variable -pedantic -std=f2008 '
                 ' -O0 -g')
     
+    target += exe_extension    
     pymake.main(srcdir, target, fc=fc, cc=cc, include_subdirs=True,
                 fflags=fflags, makeclean=not onlytouched, 
                 expedite=onlytouched, inplace=True)
  
+
 def build_mf6_dll(mode, onlytouched): 
-    
+
+    target = os.path.join('..', 'bin', 'libmf6')    
     fc = 'gfortran'
     cc = 'gcc'
-    
     srcdir = os.path.join('..', 'srcbmi')
     comdir = os.path.join('..', 'src')
     excludefiles = [os.path.join(comdir, 'mf6.f90')]
 
     if mode == "-release":
-      target = os.path.join('..', 'bin', 'libmf6.dll')
       fflags = ('-Wtabs -Wline-truncation -Wunused-label '
                   '-Wunused-variable -pedantic -std=f2008')
     else:
-      target = os.path.join('..', 'bin', 'libmf6d.dll')
+      target += 'd'
       fflags = ('-Wtabs -Wline-truncation -Wunused-label '
                   '-Wunused-variable -pedantic -std=f2008 '
                   '-O0 -g')
 
+    target += dll_extension
     pymake.main(srcdir, target, fc=fc, cc=cc, include_subdirs=True,
                 fflags=fflags, srcdir2=comdir, excludefiles=excludefiles,
-                sharedobject=True, makeclean=not onlytouched, expedite=onlytouched, inplace=True)
+                sharedobject=True, makeclean=not onlytouched, expedite=onlytouched, 
+                inplace=True)
                 
  
 def main():
