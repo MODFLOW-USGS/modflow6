9dec1367b5d4d5d5313a0b2a665f4292d3dd1483
Hughes, Joseph D <jdhughes@usgs.gov>
2021-03-16 16:57:05 -0500

    ci: build mf6 release code with the compiler being used with workflow
    Add large(long) tests workflow to GitHub Actions CI.

--- .github/common/get-examples-regression-files.sh
+++ .github/common/get-examples-regression-files.sh
@@ -3,8 +3,3 @@
 echo $HOME
 pwd
 git clone https://github.com/MODFLOW-USGS/modflow6-examples ../modflow6-examples
-cd ../modflow6-examples/etc/
-pwd
-python python ci_build_files.py
-cd ../../modflow6
-pwd
