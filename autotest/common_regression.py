2c640e20289067b28fe6df2e975f85f0b3bf0b05
jdhughes-usgs <jdhughes@usgs.gov>
2021-04-09 15:39:39 -0500

    ci: modify common_regression.py for CI runs on Windows
    black formatting on a few autotest test scripts.

--- autotest/common_regression.py
+++ autotest/common_regression.py
@@ -26,7 +26,7 @@ def get_home_dir():
 
     if is_CI:
         if sys.platform.lower() == "win32":
-            home = os.path.abspath(os.path.join(".."))
+            home = os.path.abspath(os.path.join("..", ".."))
     else:
         cwd_pth = os.getcwd()
 
