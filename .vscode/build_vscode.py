d5826c87370c2c17dc7e1cf8c2309f68b6bf4060
Hofer-Julian <julianhofer@gnome.org>
2021-10-21 12:10:41 +0200

    Let meson follow compiler options of pymake

--- .vscode/build_vscode.py
+++ .vscode/build_vscode.py
@@ -1,31 +1,26 @@
 import subprocess
 import os
 import argparse
-import sys
+import shutil
 
 parser = argparse.ArgumentParser()
-parser.add_argument('--compiler', type=str)
-parser.add_argument('action')
+parser.add_argument("--compiler", type=str)
+parser.add_argument("action")
 args = parser.parse_args()
 
 os.environ["FC"] = args.compiler
-
-
 builddir = f"builddir_{args.compiler}"
 
-subprocess.run(["meson", "setup", builddir, "--prefix", os.getcwd(), "--libdir", "bin"], check=True)
 
-if args.action == "build":
-    subprocess.run(["meson", "compile", "-C", builddir], check=True)
-elif args.action == "rebuild":
-    subprocess.run(["meson", "compile", "-C", builddir, "--clean"], check=True)
+if args.action == "rebuild":
+    if os.path.isdir(builddir):
+        shutil.rmtree(builddir)
 
-subprocess.run(
-    [
-        "meson",
-        "install",
-        "-C",
-        builddir,
-    ],
-    check=True,
-)
+    subprocess.run(
+        ["meson", "setup", builddir, "--prefix", os.getcwd(), "--libdir", "bin"],
+        check=True,
+    )
+
+subprocess.run(["meson", "compile", "-C", builddir], check=True)
+
+subprocess.run(["meson", "install", "-C", builddir], check=True)
