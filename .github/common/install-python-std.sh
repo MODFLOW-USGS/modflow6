6a801e150d814458d3ec6e2d2173c8af90fdc0bc
Hofer-Julian <julianhofer@gnome.org>
2021-10-27 10:06:06 +0200

    Add remaining meson workflows
    Adds `gfortran-previous` and `large-tests` for meson
    
    Also adds meson and ninja to `install-python-std.sh`

--- .github/common/install-python-std.sh
+++ .github/common/install-python-std.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 pip install wheel
-pip install requests appdirs numpy matplotlib pytest pytest-xdist
+pip install requests appdirs numpy matplotlib pytest pytest-xdist meson ninja
 pip install https://github.com/modflowpy/flopy/zipball/develop
 pip install https://github.com/modflowpy/pymake/zipball/master
 pip install https://github.com/Deltares/xmipy/zipball/develop
