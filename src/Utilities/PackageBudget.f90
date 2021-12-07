6b1708704eadf22465a255fa36dff273ed30b9c3
Langevin, Christian D <langevin@usgs.gov>
2021-12-07 10:07:53 -0600

    Doxygenate PackageBudget source file

--- src/Utilities/PackageBudget.f90
+++ src/Utilities/PackageBudget.f90
@@ -1,3 +1,11 @@
+!> @brief This module contains the PackageBudgetModule Module 
+!!
+!! The PackageBudgetType object defined here provides flows to the GWT
+!! model.  The PackageBudgetType can be filled with flows from a budget
+!! object that was written from a previous GWF simulation, or its 
+!! individual members can be pointed to flows that are being calculated
+!! by a GWF model that is running as part of this simulation.
+!<
 module PackageBudgetModule
   
   use KindModule
@@ -10,6 +18,12 @@ module PackageBudgetModule
   private
   public :: PackageBudgetType
   
+  !> @brief Derived type for storing flows 
+  !!
+  !! This derived type stores flows and provides them through the FMI
+  !! package to other parts of GWT.  
+  !!
+  !<
   type :: PackageBudgetType
     
     character(len=LENMEMPATH) :: memoryPath = ''                                 !< the location in the memory manager where the variables are stored
@@ -37,12 +51,17 @@ module PackageBudgetModule
   
   contains
   
+  !> @ brief Initialize a PackageBudgetType object
+  !!
+  !!  Establish the memory path and allocate and initialize member variables. 
+  !!
+  !<
   subroutine initialize(this, mempath)
-    class(PackageBudgetType) :: this
-    character(len=*), intent(in) :: mempath
+    class(PackageBudgetType) :: this           !< PackageBudgetType object
+    character(len=*), intent(in) :: mempath    !< memory path in memory manager
     this%memoryPath = mempath
     !
-    ! -- allocate scalars
+    ! -- allocate member variables in memory manager
     call mem_allocate(this%name, LENPACKAGENAME, 'NAME', mempath)
     call mem_allocate(this%budtxt, LENPACKAGENAME, 'BUDTXT', mempath)
     call mem_allocate(this%naux, 'NAUX', mempath)
@@ -60,8 +79,13 @@ module PackageBudgetModule
     return
   end subroutine initialize
   
+  !> @ brief Set names for this PackageBudgetType object
+  !!
+  !!  Set the name of the package and the name of the of budget text 
+  !!
+  !<
   subroutine set_name(this, name, budtxt)
-    class(PackageBudgetType) :: this
+    class(PackageBudgetType) :: this          !< PackageBudgetType object
     character(len=LENPACKAGENAME) :: name     !< name of the package (WEL-1, DRN-4, etc.)
     character(len=LENPACKAGENAME) :: budtxt   !< name of budget term (CHD, RCH, EVT, DRN-TO-MVR, etc.)
     this%name = name
@@ -69,18 +93,33 @@ module PackageBudgetModule
     return
   end subroutine set_name
   
+  !> @ brief Set aux names for this PackageBudgetType object
+  !!
+  !!  Set the number of auxiliary variables and the names of the 
+  !!  auxiliary variables
+  !!
+  !<
   subroutine set_auxname(this, naux, auxname)
-    class(PackageBudgetType) :: this
-    integer(I4B), intent(in) :: naux
-    character(len=LENAUXNAME), contiguous, dimension(:), intent(in) :: auxname
+    class(PackageBudgetType) :: this                  !< PackageBudgetType object
+    integer(I4B), intent(in) :: naux                  !< number of auxiliary variables
+    character(len=LENAUXNAME), contiguous, &
+                 dimension(:), intent(in) :: auxname  !< array of names for auxiliary variables
     this%naux = naux
     call mem_reallocate(this%auxname, LENAUXNAME, naux, 'AUXNAME', this%memoryPath)
     this%auxname(:) = auxname(:)
     return
   end subroutine set_auxname
   
+  !> @ brief Point members of this class to data stored in GWF packages
+  !!
+  !!  The routine is called when a GWF model is being run concurrently with 
+  !!  a GWT model.  In this situation, the member variables NBOUND, NODELIST,
+  !!  FLOW, and AUXVAR are pointed into member variables of the individual
+  !!  GWF Package members stored in BndType.
+  !!
+  !<
   subroutine set_pointers(this, flowvarname, mem_path_target)
-    class(PackageBudgetType) :: this
+    class(PackageBudgetType) :: this                  !< PackageBudgetType object
     character(len=*), intent(in) :: flowvarname       !< name of variable storing flow (SIMVALS, SIMTOMVR)
     character(len=*), intent(in) :: mem_path_target   !< path where target variable is stored
     !
@@ -96,12 +135,20 @@ module PackageBudgetModule
     return    
   end subroutine set_pointers
 
+  !> @ brief Copy data read from a budget file into this object
+  !!
+  !!  The routine is called when GWF flows are read from a budget file
+  !!  created from a previous GWF simulation.  Arrays here must be
+  !!  dynamically resized because maxbound is not known unless the
+  !!  entire budget file was read first.
+  !!
+  !<
   subroutine copy_values(this, nbound, nodelist, flow, auxvar)
-    class(PackageBudgetType) :: this
-    integer(I4B), intent(in) :: nbound
-    integer(I4B), dimension(:), contiguous, intent(in) :: nodelist
-    real(DP), dimension(:), contiguous, intent(in) :: flow
-    real(DP), dimension(:,:), contiguous, intent(in) :: auxvar
+    class(PackageBudgetType) :: this                                !< PackageBudgetType object
+    integer(I4B), intent(in) :: nbound                              !< number of entries
+    integer(I4B), dimension(:), contiguous, intent(in) :: nodelist  !< array of GWT node numbers
+    real(DP), dimension(:), contiguous, intent(in) :: flow          !< array of flow rates
+    real(DP), dimension(:,:), contiguous, intent(in) :: auxvar      !< array of auxiliary variables
     integer(I4B) :: i
     !
     ! -- Assign variables
@@ -123,14 +170,24 @@ module PackageBudgetModule
     end do
   end subroutine copy_values
   
+  !> @ brief Get flow rate for specified entry
+  !!
+  !!  Return the flow rate for the specified entry
+  !!
+  !<
   function get_flow(this, i) result(flow)
-    class(PackageBudgetType) :: this
-    integer(I4B), intent(in) :: i
+    class(PackageBudgetType) :: this   !< PackageBudgetType object
+    integer(I4B), intent(in) :: i      !< entry number
     real(DP) :: flow
     flow = this%flow(i)
     return
   end function get_flow
   
+  !> @ brief Deallocate
+  !!
+  !!  Free any memory associated with this object
+  !!
+  !<
   subroutine da(this)
     class(PackageBudgetType) :: this
     call mem_deallocate(this%name, 'NAME', this%memoryPath)
@@ -143,7 +200,5 @@ module PackageBudgetModule
     call mem_deallocate(this%auxvar, 'AUXVAR', this%memoryPath)
     return
   end subroutine da
-  
-  
   
 end module PackageBudgetModule
