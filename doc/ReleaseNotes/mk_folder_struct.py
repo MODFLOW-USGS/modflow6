50a29fc656b2f106f01411c39834aa5f0947e6bc
langevin-usgs <langevin@usgs.gov>
2022-02-01 12:33:49 -0600

    docs(releasenotes): clean up mk_folder_struct.py (#874)
    * clean up mk_folder_struct.py
    * allow idevelop to be set as 0 or 1 from command line in make_release.py

--- doc/ReleaseNotes/mk_folder_struct.py
+++ doc/ReleaseNotes/mk_folder_struct.py
@@ -1,6 +1,20 @@
+#!/usr/bin/python
+
+"""
+mk_folder_struct.py: Create a latex file containing structure of a folder.
+
+This script is used to update folder_struct.tex using the structure of a specified
+distribution folder.  The distribution folder must be specified using a command line
+argument.  Usage is:
+
+  python mk_folder_struct.py -dp <distribution path>
+
+"""
+
 
 import os
 import sys
+
 
 def list_files(startpath, includefiles=False):
     s = ''
@@ -14,47 +28,30 @@ def list_files(startpath, includefiles=False):
                 s += '{}{} \n'.format(subindent, f)
     return s
 
-def get_distribution_name(versiontexname):
-    dist = None
-    fname = versiontexname
-    with open(fname) as f:
-        lines = f.readlines()
-        f.close()
-    for line in lines:
-        # \newcommand{\modflowversion}{mf6beta0.9.00}
-        srchtxt = '\\newcommand{\\modflowversion}'
-        istart = line.rfind('{') + 1
-        istop = line.rfind('}')
-        if 0 < istart < istop:
-            dist = line[istart: istop]
-            return dist
-    return None
+
+def get_distribution_path():
+    distribution_path = None
+    for idx, arg in enumerate(sys.argv):
+        if arg == "-dp":
+            distribution_path = sys.argv[idx + 1]
+    return distribution_path
 
 
-pth = os.path.join('..', '..')
-if pth not in sys.path:
-    sys.path.append(pth)
-
-# Find version information
-fname = os.path.join('..', 'version.tex')
-dist = get_distribution_name(fname)
-
-# Set up the path to the distribution
-#dist = 'mf6beta.0.6.00'
-distpth = os.path.join('..', '..', 'distribution', dist)
-if not os.path.isdir(distpth):
-    raise Exception(distpth + ' does not exist. ')
+distribution_path = get_distribution_path()
+if distribution_path is None:
+    raise Exception(f"Distribution path must be set from command line using -dp <dp>")
+if not os.path.isdir(distribution_path):
+    raise Exception(f"Distribution path not found: {distribution_path}")
 
 # Open the file
 f = open('folder_struct.tex', 'w')
     
 # Write the latex header
-
 s = r'''\begin{verbatim}'''
 f.write(s)
 f.write('\n')
 
-s = list_files(distpth)
+s = list_files(distribution_path)
 f.write(s)
 
 s = r'''\end{verbatim}
