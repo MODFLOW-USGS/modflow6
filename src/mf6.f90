636828f4f643e838f6a47fb64078716a58fcbc7b
Julian Hofer <julianhofer@gnome.org>
2022-02-04 14:16:01 +0100

    Initialize PETSc

--- src/mf6.f90
+++ src/mf6.f90
@@ -6,9 +6,21 @@
 program mf6
   ! -- modules
   use Mf6CoreModule
+#include <petsc/finclude/petscsys.h>
+  use petscsys
+
+  PetscErrorCode ierr
+
+  call PetscInitialize(PETSC_NULL_CHARACTER,ierr)
+  if (ierr .ne. 0) then
+    print*,'Unable to initialize PETSc'
+    stop
+  endif
   !
   ! -- run
   call Mf6Run()
+
+  call PetscFinalize(ierr)
   
 end program
   
