719755b6189b80ae1ed2384f5bfbc9575efcdc6e
Scott Paulinski <spaulinski@usgs.gov>
2022-04-18 12:42:55 -0700

    remove test

--- .github/common/install-python-std.sh
+++ .github/common/install-python-std.sh
@@ -2,7 +2,7 @@
 
 pip install wheel
 pip install requests appdirs numpy matplotlib pytest pytest-xdist meson ninja
-pip install https://github.com/scottrp/flopy/zipball/develop
+pip install https://github.com/modflowpy/flopy/zipball/develop
 pip install https://github.com/modflowpy/pymake/zipball/master
 pip install https://github.com/Deltares/xmipy/zipball/develop
 pip install https://github.com/MODFLOW-USGS/modflowapi/zipball/develop
