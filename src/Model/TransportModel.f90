5c67670ab26defae4640ff467aaa8ab67db8ae26
Eric Morway <emorway@usgs.gov>
2022-05-23 07:11:36 -0700

    Update header comments

--- src/Model/TransportModel.f90
+++ src/Model/TransportModel.f90
@@ -1,11 +1,8 @@
-! Generalized Transport Base Class
-! Base class for solute (mass) and energy (thermal) transport
-!   (The following copied from gwt1.f90)
-!   * Add check that discretization is the same between both models 
-!   * Program GWT-GWT exchange transport (awaiting implementation of interface model)
-!   * Consider implementation of steady-state transport (affects MST, IST)
-!   * Check and handle pore space discrepancy between flow and transport (porosity vs specific yield)
-!   * UZT may not have the required porosity term
+!> @brief This module contains the base transport model type
+!!
+!! This module contains the base class for transport models.
+!!
+!<
 
 module TransportModelModule
   use KindModule,                  only: DP, I4B
