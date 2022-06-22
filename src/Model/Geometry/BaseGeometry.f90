7494a7f914e5382ab9b8cbc9c1a663e980d7a5af
mjreno <mjreno@ncsu.edu>
2022-06-22 17:30:21 -0400

    style(geometry): apply fprettify formatting
    * working toward consistent code formatting

--- src/Model/Geometry/BaseGeometry.f90
+++ src/Model/Geometry/BaseGeometry.f90
@@ -1,13 +1,13 @@
 module BaseGeometryModule
-  
+
   use KindModule, only: DP, I4B
-  
+
   implicit none
   private
   public BaseGeometryType
-  
+
   integer(I4B), parameter :: GEONAMELEN = 20
-  
+
   type :: BaseGeometryType
     character(len=20) :: geo_type = 'UNDEFINED'
     integer(I4B) :: id = 0
@@ -21,8 +21,8 @@ module BaseGeometryModule
     procedure :: print_attributes
   end type BaseGeometryType
 
-  contains
-  
+contains
+
   function area_sat(this)
     ! -- return
     real(DP) :: area_sat
@@ -32,8 +32,8 @@ module BaseGeometryModule
     ! -- return
     return
   end function area_sat
-  
- function perimeter_sat(this)
+
+  function perimeter_sat(this)
     ! -- return
     real(DP) :: perimeter_sat
     ! -- dummy
@@ -42,29 +42,29 @@ module BaseGeometryModule
     ! -- return
     return
   end function perimeter_sat
-  
+
   function area_wet(this, depth)
     ! -- return
     real(DP) :: area_wet
     ! -- dummy
     class(BaseGeometryType) :: this
-    real(DP), intent(in) :: depth    
+    real(DP), intent(in) :: depth
     area_wet = 0.d0
     ! -- return
     return
   end function area_wet
-  
+
   function perimeter_wet(this, depth)
     ! -- return
     real(DP) :: perimeter_wet
     ! -- dummy
     class(BaseGeometryType) :: this
-    real(DP), intent(in) :: depth    
+    real(DP), intent(in) :: depth
     perimeter_wet = 0.d0
     ! -- return
     return
   end function perimeter_wet
-  
+
   subroutine set_attribute(this, line)
     ! -- dummy
     class(BaseGeometryType) :: this
@@ -72,7 +72,7 @@ module BaseGeometryModule
     ! -- return
     return
   end subroutine set_attribute
-  
+
   subroutine print_attributes(this, iout)
 ! ******************************************************************************
 ! print_attributes -- print the attributes for this object
@@ -89,13 +89,12 @@ module BaseGeometryModule
     character(len=*), parameter :: fmtnm = "(4x,a,a)"
 ! ------------------------------------------------------------------------------
     !
-    write(iout, fmtid) 'ID = ', this%id
-    write(iout, fmtnm) 'NAME = ', trim(adjustl(this%name))
-    write(iout, fmtnm) 'GEOMETRY TYPE = ', trim(adjustl(this%geo_type))
+    write (iout, fmtid) 'ID = ', this%id
+    write (iout, fmtnm) 'NAME = ', trim(adjustl(this%name))
+    write (iout, fmtnm) 'GEOMETRY TYPE = ', trim(adjustl(this%geo_type))
     !
     ! -- return
     return
   end subroutine print_attributes
-  
-  
+
 end module BaseGeometryModule
