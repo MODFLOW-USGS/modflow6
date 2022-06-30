792f6dbf9077df3a1ac78e513d92177a8252bc66
mjreno <mjreno@ncsu.edu>
2022-06-30 18:24:02 -0400

    fix(ci): workaround for windows-2022 gfortran executable run failures

--- .github/common/link-gfortranlib5.sh
+++ .github/common/link-gfortranlib5.sh
@@ -2,6 +2,6 @@
 
 FCDIR=/c/ProgramData/Chocolatey/bin
 LNDIR=/c/ProgramData/Chocolatey/lib/mingw/tools/install/mingw64/bin
-if [ -d "$FCDIR" ] && [ -d "$LNDIR" ] && [ ! -f "$FCDIR/libgfortran-5.dll" ]; then
+if [ -d "$FCDIR" ] && [ -f "$LNDIR/libgfortran-5.dll" ] && [ ! -f "$FCDIR/libgfortran-5.dll" ]; then
     ln -s "$LNDIR/libgfortran-5.dll" "$FCDIR/libgfortran-5.dll"
 fi
