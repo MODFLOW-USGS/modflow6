140917e2929253054856bc1a3b786dea494c7e9c
Joseph Hughes <jdhughes@usgs.gov>
2022-07-06 09:20:32 -0500

    ci: fix Intel OneAPI build on Windows
    The issue is caused by the latest meson release. Exclude meson version 0.63.0.

--- .github/common/install-python-std.sh
+++ .github/common/install-python-std.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 pip install wheel
-pip install requests appdirs numpy matplotlib pytest pytest-xdist meson ninja
+pip install requests appdirs numpy matplotlib pytest pytest-xdist meson!=0.63.0 ninja
 pip install https://github.com/modflowpy/flopy/zipball/develop
 pip install https://github.com/modflowpy/pymake/zipball/master
 pip install https://github.com/Deltares/xmipy/zipball/develop
