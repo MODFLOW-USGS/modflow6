fd3e5e1158e5733d1b486d4c7b240265af149ab9
Eric Morway <emorway@usgs.gov>
2022-07-13 16:48:30 -0700

    Updating the contents of the src/Model/Connection directory with the fprettify stuff.  2 files that I wrote need to have fprettify applied (GweGweConnection and GweInterfaceModel) and 1 file that I modified (GwtInterfaceModel.f90)

--- src/Model/Connection/GridSorting.f90
+++ src/Model/Connection/GridSorting.f90
@@ -1,4 +1,4 @@
-module GridSorting  
+module GridSorting
   use KindModule, only: I4B, DP, LGP
   use ConstantsModule, only: DHALF
   use CellWithNbrsModule, only: GlobalCellType
@@ -11,74 +11,76 @@ module GridSorting
 contains
   ! Sort an array of integers
   subroutine quickSortGrid(array, arraySize, idxToGlobal)
-      integer, intent(inout), dimension(:) :: array      
-      integer, intent(in) :: arraySize
-      type(GlobalCellType), dimension(:), pointer :: idxToGlobal
+    integer, intent(inout), dimension(:) :: array
+    integer, intent(in) :: arraySize
+    type(GlobalCellType), dimension(:), pointer :: idxToGlobal
+    ! local
+    integer :: QSORT_THRESHOLD = 8
+    include "qsort_inline.inc"
+
+  contains
+    subroutine init()
+    end subroutine init
+
+    ! Compare two grid cells, this doesn't work as
+    ! smooth for staggered discretizations though...
+    function lessThan(n, m) result(isLess)
+      integer(I4B), intent(in) :: n
+      integer(I4B), intent(in) :: m
+      logical(LGP) :: isLess
       ! local
-      integer :: QSORT_THRESHOLD = 8
-      include "qsort_inline.inc"
-          
-      contains
-        subroutine init()
-        end subroutine init
+      type(GlobalCellType), pointer :: gcn, gcm
+      real(DP) :: xnloc, ynloc, xmloc, ymloc
+      real(DP) :: xn, yn, zn, xm, ym, zm
 
-        ! Compare two grid cells, this doesn't work as
-        ! smooth for staggered discretizations though...
-        function lessThan(n, m) result(isLess)
-          integer(I4B), intent(in) :: n
-          integer(I4B), intent(in) :: m
-          logical(LGP) :: isLess
-          ! local
-          type(GlobalCellType), pointer :: gcn, gcm
-          real(DP) :: xnloc, ynloc, xmloc, ymloc
-          real(DP) :: xn, yn, zn, xm, ym, zm
-            
-          ! get coordinates
-          gcn => idxToGlobal(array(n))
-          gcm => idxToGlobal(array(m))
-  
-          ! convert coordinates
-          call gcn%model%dis%get_cellxy(gcn%index, xnloc, ynloc)
-          call gcn%model%dis%transform_xy(xnloc, ynloc, xn, yn)
-          zn = DHALF*(gcn%model%dis%top(gcn%index) + gcn%model%dis%bot(gcn%index))
-  
-          call gcm%model%dis%get_cellxy(gcm%index, xmloc, ymloc)
-          call gcm%model%dis%transform_xy(xmloc, ymloc, xm, ym)
-          zm = DHALF*(gcm%model%dis%top(gcm%index) + gcm%model%dis%bot(gcm%index))
-  
-          ! compare
-          if (.not. is_same(zn, zm, 10*epsilon(zn))) then
-            isLess = zn > zm
-          else if (.not. is_same(yn, ym, 10*epsilon(yn))) then
-            isLess = yn > ym
-          else if (.not. is_same(xn, xm, 10*epsilon(xn))) then
-            isLess = xn < xm
-          else
-            isLess = .false.
-          end if
+      ! get coordinates
+      gcn => idxToGlobal(array(n))
+      gcm => idxToGlobal(array(m))
 
-        end function lessThan
+      ! convert coordinates
+      call gcn%model%dis%get_cellxy(gcn%index, xnloc, ynloc)
+      call gcn%model%dis%transform_xy(xnloc, ynloc, xn, yn)
+      zn = DHALF * (gcn%model%dis%top(gcn%index) + &
+                    gcn%model%dis%bot(gcn%index))
 
-        ! swap indices
-        subroutine swap(a,b)
-          integer, intent(in) :: a,b
-          integer :: hold
+      call gcm%model%dis%get_cellxy(gcm%index, xmloc, ymloc)
+      call gcm%model%dis%transform_xy(xmloc, ymloc, xm, ym)
+      zm = DHALF * (gcm%model%dis%top(gcm%index) + &
+                    gcm%model%dis%bot(gcm%index))
 
-          hold=array(a)
-          array(a)=array(b)
-          array(b)=hold
+      ! compare
+      if (.not. is_same(zn, zm, 10 * epsilon(zn))) then
+        isLess = zn > zm
+      else if (.not. is_same(yn, ym, 10 * epsilon(yn))) then
+        isLess = yn > ym
+      else if (.not. is_same(xn, xm, 10 * epsilon(xn))) then
+        isLess = xn < xm
+      else
+        isLess = .false.
+      end if
 
-        end subroutine swap
-        
-        ! circular shift-right by one
-        subroutine rshift(left,right)
-          integer, intent(in) :: left, right
-          integer :: hold
+    end function lessThan
 
-          hold=array(right)
-          array(left+1:right)=array(left:right-1)
-          array(left)=hold
+    ! swap indices
+    subroutine swap(a, b)
+      integer, intent(in) :: a, b
+      integer :: hold
 
-        end subroutine rshift
+      hold = array(a)
+      array(a) = array(b)
+      array(b) = hold
+
+    end subroutine swap
+
+    ! circular shift-right by one
+    subroutine rshift(left, right)
+      integer, intent(in) :: left, right
+      integer :: hold
+
+      hold = array(right)
+      array(left + 1:right) = array(left:right - 1)
+      array(left) = hold
+
+    end subroutine rshift
   end subroutine quickSortGrid
 end module GridSorting
