ced185c07497cc5e4584176ee96c2f10a4ae9d91
mjr-deltares <martijn.russcher@deltares.nl>
2022-10-03 09:23:26 +0200

    fixing merge error

--- src/Model/GroundWaterTransport/gwt1dsp.f90
+++ src/Model/GroundWaterTransport/gwt1dsp.f90
@@ -142,10 +142,6 @@ contains
       !
       ! -- Read dispersion data
       call this%read_data()
-      call this%allocate_arrays(this%dis%nodes)
-      !
-      ! -- Read dispersion data
-      call this%read_data()
     end if
     !
     ! -- xt3d create
