2361f32a3e08aae98c6e8603a10a700c883802e7
langevin-usgs <langevin@usgs.gov>
2023-02-14 16:11:45 -0600

    fix(NPF): negative icelltype without THICKSTRT produced unexpected results (#1147)
    * The ICELLTYPE input variable in the Node Property Flow (NPF) Package behaves differently depending on whether or not the THICKSTRT option is specified by the user.  In some cases, the program would give unexpected results if a negative value was specified for ICELLTYPE and the THICKSTRT option was not active.  For example, the Horizontal Flow Barrier (HFB) Package did not work property when negative values for ICELLTYPE were specified by the user, but the THICKSTRT option was not activated.  The program was modified so that negative ICELLTYPE values provided by the user are automatically reassigned a value of one when the user does not activate the THICKSTRT option.  This is the intended behavior and is consistent with the input and output guide.
    * close #1146

---
+++ autotest/test_gwf_npf_thickstrt.py
@@ -0,0 +1,216 @@
+import os
+
+import flopy
+import numpy as np
+import pytest
+from framework import TestFramework
+from simulation import TestSimulation
+
+ex = [
+    "gwf_npf_thickstrt01",  # case 01 -- icelltype=0
+    "gwf_npf_thickstrt02",  # case 02 -- icelltype=0, using thickstrt, but it has no effect
+    "gwf_npf_thickstrt03",  # case 03 -- icelltype=-1, using thickstrt and strt = 5.
+    "gwf_npf_thickstrt04",  # case 04 -- icelltype=1, no thickstrt and strt = 5.
+    "gwf_npf_thickstrt05",  # case 05 -- icelltype=-1, no thickstrt and strt = 5.
+    "gwf_npf_thickstrt06",  # case 06 -- icelltype=0, no thickstrt, has hfb
+    "gwf_npf_thickstrt07",  # case 07 -- icelltype=-1, using thickstrt, has hfb
+    "gwf_npf_thickstrt08",  # case 08 -- icelltype=1, no thickstrt, has hfb
+    "gwf_npf_thickstrt09",  # case 09 -- icelltype=-1, no thickstrt, has hfb
+]
+thickstrt = [False, True, True, False, False, False, True, False, False]
+icelltype = [0, 0, -1, 1, -1, 0, -1, 1, -1]
+hfb_on = [False, False, False, False, False, True, True, True, True]
+
+def build_model(idx, dir):
+    nlay, nrow, ncol = 1, 1, 6
+    nper = 1
+    perlen = [1.0]
+    nstp = [1]
+    tsmult = [1.0]
+    delr = 1.0
+    delc = 1.0
+    top = 10.0
+    botm = [0.0]
+    strt = 5.0
+    hk = 1.0
+
+    c = {0: [[(0, 0, 0), 6.0], [(0, 0, ncol - 1), 4.0]]}
+
+    nouter, ninner = 10, 5
+    hclose, rclose, relax = 1e-6, 1e-6, 1.0
+
+    tdis_rc = []
+    for i in range(nper):
+        tdis_rc.append((perlen[i], nstp[i], tsmult[i]))
+
+    name = "flow"
+
+    # build MODFLOW 6 files
+    ws = dir
+    sim = flopy.mf6.MFSimulation(
+        sim_name=name, version="mf6", exe_name="mf6", sim_ws=ws
+    )
+    # create tdis package
+    tdis = flopy.mf6.ModflowTdis(
+        sim, time_units="DAYS", nper=nper, perioddata=tdis_rc
+    )
+
+    # create gwf model
+    gwf = flopy.mf6.MFModel(
+        sim,
+        model_type="gwf6",
+        modelname=name,
+        model_nam_file=f"{name}.nam",
+    )
+    gwf.name_file.save_flows = True
+
+    # create iterative model solution and register the gwf model with it
+    imsgwf = flopy.mf6.ModflowIms(
+        sim,
+        print_option="SUMMARY",
+        outer_dvclose=hclose,
+        outer_maximum=nouter,
+        under_relaxation="NONE",
+        inner_maximum=ninner,
+        inner_dvclose=hclose,
+        rcloserecord=rclose,
+        linear_acceleration="CG",
+        scaling_method="NONE",
+        reordering_method="NONE",
+        relaxation_factor=relax,
+        filename=f"{name}.ims",
+    )
+    sim.register_ims_package(imsgwf, [gwf.name])
+
+    dis = flopy.mf6.ModflowGwfdis(
+        gwf,
+        nlay=nlay,
+        nrow=nrow,
+        ncol=ncol,
+        delr=delr,
+        delc=delc,
+        top=top,
+        botm=botm,
+        idomain=np.ones((nlay, nrow, ncol), dtype=int),
+    )
+
+    # initial conditions
+    ic = flopy.mf6.ModflowGwfic(gwf, strt=strt)
+
+    # npf
+    thickstrt_option = thickstrt[idx]
+    ict = icelltype[idx]
+    npf = flopy.mf6.ModflowGwfnpf(
+        gwf, 
+        thickstrt=thickstrt_option,
+        icelltype=ict, 
+        k=hk, 
+        k33=hk
+    )
+
+    if hfb_on[idx]:
+        hfb = flopy.mf6.ModflowGwfhfb(
+            gwf,
+            print_input=True,
+            maxhfb=1,
+            stress_period_data=[((0, 0, 2), (0, 0, 3), 1.e-4)],
+        )
+
+    # chd files
+    chd = flopy.mf6.ModflowGwfchd(
+        gwf,
+        maxbound=len(c),
+        stress_period_data=c,
+        save_flows=False,
+        print_flows=True,
+        pname="CHD-1",
+    )
+
+    # output control
+    oc = flopy.mf6.ModflowGwfoc(
+        gwf,
+        budget_filerecord=f"{name}.cbc",
+        head_filerecord=f"{name}.hds",
+        headprintrecord=[("COLUMNS", 10, "WIDTH", 15, "DIGITS", 6, "GENERAL")],
+        saverecord=[("HEAD", "LAST"), ("BUDGET", "LAST")],
+        printrecord=[("HEAD", "LAST"), ("BUDGET", "LAST")],
+    )
+
+    return sim, None
+
+
+def eval_model(sim):
+    print("evaluating model...")
+
+    name = "flow"
+
+    fpth = os.path.join(sim.simpath, f"{name}.hds")
+    hobj = flopy.utils.HeadFile(fpth, precision="double")
+    head = hobj.get_data().flatten()
+
+    # This is the answer to this problem.
+    answer_linear = np.linspace(6, 4, 6)
+
+    answer_water_table = (6.0, 5.65716, 5.29206, 4.89969, 4.47276, 4.0)
+    answer_water_table = np.array(answer_water_table)
+
+    answer_confined_hfb = (6.0, 5.9998, 5.9996, 4.0004, 4.0002, 4.0)
+    answer_confined_hfb = np.array(answer_confined_hfb)
+
+    answer_confined_thickstart_hfb = (6., 5.9996004, 5.9992008, 4.0007992, 4.0003996, 4.)
+    answer_confined_thickstart_hfb = np.array(answer_confined_thickstart_hfb)
+
+    answer_unconfined_hfb = (6., 5.99983342, 5.99966683, 4.00049971, 4.00024986, 4.)
+    answer_unconfined_hfb = np.array(answer_unconfined_hfb)
+
+    answer_dict = {
+        0: answer_linear, 
+        1: answer_linear, 
+        2: answer_linear, 
+        3: answer_water_table, 
+        4: answer_water_table, 
+        5: answer_confined_hfb,
+        6: answer_confined_thickstart_hfb,
+        7: answer_unconfined_hfb,
+        8: answer_unconfined_hfb,
+    }
+
+    hres = answer_dict[sim.idxsim]
+    assert np.allclose(
+        hres, head
+    ), "simulated head do not match with known solution."
+
+    fpth = os.path.join(sim.simpath, f"{name}.cbc")
+    cobj = flopy.utils.CellBudgetFile(fpth, precision="double")
+    q_simulated_inflow = cobj.get_data(idx=1)[0]['q'][0]
+    q_answer_dict = {
+        0: 4.,
+        1: 4.,
+        2: 2.,
+        3: 1.9965396769631871,
+        4: 1.9965396769631871,
+        5: 1.9990E-03,
+        6: 1.9980E-03,
+        7: 9.9949E-04,
+        8: 9.9949E-04,
+    }
+    q_answer = q_answer_dict[sim.idxsim]
+    assert np.allclose(
+        q_answer, q_simulated_inflow
+    ), "simulated flow does not match with known solution."
+
+
+@pytest.mark.parametrize(
+    "idx, name",
+    list(enumerate(ex)),
+)
+def test_mf6model(idx, name, function_tmpdir, targets):
+    ws = str(function_tmpdir)
+    test = TestFramework()
+    test.build(build_model, idx, ws)
+    test.run(
+        TestSimulation(
+            name=name, exe_dict=targets, exfunc=eval_model, idxsim=idx
+        ),
+        ws,
+    )
