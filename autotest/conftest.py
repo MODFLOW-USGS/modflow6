1ab1526e8abdeffe1291feb09a5318c168674455
mjr-deltares <martijn.russcher@deltares.nl>
2023-02-25 08:52:02 +0100

    should be only 1 pytest_addoption...

--- autotest/conftest.py
+++ autotest/conftest.py
@@ -49,11 +49,13 @@ def pytest_addoption(parser):
         default=False,
         help="TODO",
     )
-
-def pytest_addoption(parser):
     parser.addoption(
-        "--parallel", action="store_true", default=False, help="include parallel test cases"
+        "--parallel", 
+        action="store_true", 
+        default=False, 
+        help="include parallel test cases"
     )
+
 
 def pytest_collection_modifyitems(config, items):
     if config.getoption("--parallel"):
