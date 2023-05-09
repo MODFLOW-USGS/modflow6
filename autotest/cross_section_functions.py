fix(SfrCrossSectionUtils): fix for n-point x-section area calculation (#1172)
* fix(SfrCrossSectionUtils): fix for n-point x-section area calculation

* Changes that accounted for the vertically wetted sides in the wide rectangular channel approach should not have been made.

* updates I originally forgot to transfer when redoing the PR (originally #1134)

* now that the areas are being calculated correctly, really small widths are being multiplied by really small depths on the intial guess at channel depth. As a result, the iterative stage calculation blows up leading to a temporary fix.

* one more fix I should've included earlier

* goes with e579d10

* More work to do on this PR after conversation with @jdhughes-usgs

* clean-up python to go with reworked fortran changes.

* Bolster discussion in 'Streamflow Routing Package Cross-SectionTable Input File' section as discussed.

* Update doc/mf6io/gwf/sfr.tex

Co-authored-by: langevin-usgs <langevin@usgs.gov>

* remove errant "("

* more changes after checking answers for a more complex x-section; reran black

---------

Co-authored-by: langevin-usgs <langevin@usgs.gov>

autotest/cross_section_functions.py
@@ -12,7 +12,7 @@ def calculate_rectchan_mannings_discharge(
 
     """
     area = width * depth
-    return conversion_factor * area * depth**mpow * slope**0.5 / roughness
+    return conversion_factor * area * depth ** mpow * slope ** 0.5 / roughness
 
 
 # n-point cross-section functions
@@ -69,17 +69,20 @@ def get_wetted_perimeter(
 
     # -- calculate the wetted perimeter for the segment
     xlen = x1 - x0
+    dlen = 0.0
     if xlen > 0.0:
-        if depth > hmax:
+        if depth >= hmax:
             dlen = hmax - hmin
         else:
             dlen = depth - hmin
+            xlen = x1 - x0
     else:
         if depth > hmin:
             dlen = min(depth, hmax) - hmin
         else:
             dlen = 0.0
-    return np.sqrt(xlen**2.0 + dlen**2.0)
+
+    return np.sqrt(xlen ** 2.0 + dlen ** 2.0)
 
 
 def get_wetted_area(x0, x1, h0, h1, depth):
@@ -95,8 +98,12 @@ def get_wetted_area(x0, x1, h0, h1, depth):
         if depth > hmax:
             area = xlen * (depth - hmax)
         # -- add the area below zmax
-        if hmax != hmin and depth > hmin:
-            area += 0.5 * (depth - hmin)
+        if hmax != hmin:
+            if depth >= hmax:
+                area += 0.5 * (hmax - hmin) * xlen
+            elif depth > hmin:
+                x0, x1 = get_wetted_station(x0, x1, h0, h1, depth)
+                area += 0.5 * (depth - hmin) * (x1 - x0)
     return area
 
 
@@ -113,9 +120,6 @@ def wetted_area(
         for idx in range(0, x.shape[0] - 1):
             x0, x1 = x[idx], x[idx + 1]
             h0, h1 = h[idx], h[idx + 1]
-
-            # get station data
-            x0, x1 = get_wetted_station(x0, x1, h0, h1, depth)
 
             # get wetted area
             a = get_wetted_area(x0, x1, h0, h1, depth)
@@ -172,12 +176,16 @@ def manningsq(
         q = 0.0
         for i0 in range(x.shape[0] - 1):
             i1 = i0 + 1
-            perimeter = get_wetted_perimeter(x[i0], x[i1], h[i0], h[i1], depth)
+
+            # get station data
+            x0, x1 = get_wetted_station(x[i0], x[i1], h[i0], h[i1], depth)
+
+            perimeter = get_wetted_perimeter(x0, x1, h[i0], h[i1], depth)
             area = get_wetted_area(x[i0], x[i1], h[i0], h[i1], depth)
             if perimeter > 0.0:
                 radius = area / perimeter
                 q += (
-                    conv * area * radius**mpow * slope**0.5 / roughness[i0]
+                    conv * area * radius ** mpow * slope ** 0.5 / roughness[i0]
                 )
     else:
         perimeter = wetted_perimeter(x, h, depth)
@@ -185,7 +193,7 @@ def manningsq(
         radius = 0.0
         if perimeter > 0.0:
             radius = area / perimeter
-        q = conv * area * radius**mpow * slope**0.5 / roughness[0]
+        q = conv * area * radius ** mpow * slope ** 0.5 / roughness[0]
     return q
 
 

autotest/test_gwf_sfr_npoint02.py
@@ -5,6 +5,7 @@ import numpy as np
 import pytest
 from framework import TestFramework
 from simulation import TestSimulation
+from cross_section_functions import get_depths
 
 paktest = "sfr"
 
@@ -46,6 +47,7 @@ for n in range(nper):
         "h": np.array([0.0, 0.0], dtype=float),
     }
 
+
 # depth as a function of flow for a wide cross-section
 def flow_to_depth_wide(rwid, q):
     return ((q * roughness) / (conversion_fact * rwid * np.sqrt(slope))) ** 0.6
@@ -53,7 +55,6 @@ def flow_to_depth_wide(rwid, q):
 
 #
 def build_model(idx, ws):
-
     # build MODFLOW 6 files
     name = ex[idx]
     sim = flopy.mf6.MFSimulation(
@@ -219,10 +220,22 @@ def eval_npointdepth(sim):
         obs["INFLOW"], np.abs(obs["OUTFLOW"])
     ), "inflow not equal to outflow"
 
-    d = flow_to_depth_wide(
-        obs["WIDTH"],
-        inflow,
-    )
+    d = []
+    for n in range(nper):
+        x0 = 0.0
+        x1 = rwid * (n + 1)  # generates absolute widths generated above
+        x = np.array([x0, x1])
+        cdepth = get_depths(
+            inflow,
+            x=x,
+            h=np_data[n]["h"],
+            roughness=roughness,
+            slope=slope,
+            conv=1.0,
+            dd=1e-4,
+            verbose=False,
+        )
+        d.append(cdepth[0])
 
     assert np.allclose(
         obs["DEPTH"], d

autotest/test_gwf_sfr_npoint03.py
@@ -59,8 +59,16 @@ np_data = {
     "r": np.array([10.0, 1.0, 10.0, 10.0], dtype=float),
 }
 
-
+#  Cross section depiction
 #
+#    |           |           |           |
+#    |  (left)   | (channel) |  (right)  |
+#    |   10.0    |    1.0    |   10.0    | <- "MANFRACTION"
+#    |           |           |           |
+#    +-----------+-----------+-----------+  y: 0, 0, 0, 0
+# x: 0          1/3         2/3          1
+#
+
 def build_model(idx, ws, base=False):
 
     if base: