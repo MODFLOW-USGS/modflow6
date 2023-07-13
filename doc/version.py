b743eef3b2c30512d13a20255a10f502a4863065
w-bonelli <wbonelli@uga.edu>
2023-07-13 15:34:05 -0400

    update version to 6.5.0.dev0

--- doc/version.py
+++ doc/version.py
@@ -1,3 +1,3 @@
 # MODFLOW 6 version file automatically created using...update_version.py
-# created on...June 29, 2023 20:56:14
-__version__ = '6.4.3.dev0'
+# created on...July 13, 2023 15:32:29
+__version__ = "6.5.0.dev0"
