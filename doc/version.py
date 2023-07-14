697b889b64cd3b527c22ead7eef5279d51bdda3a
w-bonelli <wbonelli@ucar.edu>
2023-07-14 15:58:33 -0400

    ci(release): reinit release notes, update dist scripts (#1283)
    * reinit release notes for next development cycle
    * add --repo-owner option to build_docs.py
    * use .mf6minsim model for sample mf6 output in docs
    * refactor update_version.py to use packaging.version
    * convert version.txt to plain text
    * convert development version to v6.4.3.dev0
    * include ostag in versions subbed into docs
    * check dist subdirs in check_dist.py
    * include meson.options in full dist

--- doc/version.py
+++ doc/version.py
@@ -1,10 +1,3 @@
 # MODFLOW 6 version file automatically created using...update_version.py
-# created on...June 29, 2023 00:01:07
-
-major = 6
-minor = 4
-micro = 2
-label = '+'
-__version__ = '{:d}.{:d}.{:d}'.format(major, minor, micro)
-if label:
-	__version__ += '{}{}'.format(__version__, label)
+# created on...July 13, 2023 15:32:29
+__version__ = "6.5.0.dev0"
