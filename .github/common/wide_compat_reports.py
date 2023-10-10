a89dcd4cd52adc1e0d1c60a4953304e588b80845
w-bonelli <wbonelli@ucar.edu>
2023-10-10 13:53:16 -0400

    workaround until ubuntu-20.04 and ifx/ifort combo is testable

--- .github/common/wide_compat_reports.py
+++ .github/common/wide_compat_reports.py
@@ -9,18 +9,42 @@ import sys
 
 ip = Path(sys.argv[1])  # input file path
 op = Path(sys.argv[2])  # output file path
-
 assert ip.is_file()
 assert ip.suffix == ".csv"
 assert op.suffix == ".csv"
 
+# read long CSV
+df = pd.read_csv(ip)
+
+# workaround until intel compilers are installable on ubuntu-20.04 via setup-fortran
+ub20_ifort = df[
+    (df["compiler"] == "intel-classic") & (df["runner"] == "macos-12")
+].copy()
+ub20_ifort["runner"] = "ubuntu-20.04"
+ub20_ifort["support"] = "?"
+ub20_ifort[
+    (ub20_ifort["runner"] == "ubuntu-20.04") & (ub20_ifort["version"] == 2021.7)
+]["support"] = "&check;"
+
+ub20_ifx = df[(df["compiler"] == "intel") & (df["runner"] == "ubuntu-22.04")].copy()
+ub20_ifx["runner"] = "ubuntu-20.04"
+ub20_ifx["support"] = "?"
+
+
+df = pd.concat([df, ub20_ifort, ub20_ifx])
+
+# pivot and sort
 df = pd.pivot(
-    pd.read_csv(ip),
+    df,
     index="runner",
     columns=["compiler", "version"],
     values="support",
 ).sort_values(by=["runner"])
+
+# write wide CSV
 df.to_csv(op)
+
+# write wide markdown table
 with open(op.with_suffix(".md"), "w") as file:
     file.write(
         df.to_markdown()
