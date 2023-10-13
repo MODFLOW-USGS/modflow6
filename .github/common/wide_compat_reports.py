dc00c38e0fef8a01da7a8b004b88f7f329648965
Wes Bonelli <wpb3hw@virginia.edu>
2023-10-13 08:42:13 -0400

    remove ubuntu-20.04 intel workarounds

--- .github/common/wide_compat_reports.py
+++ .github/common/wide_compat_reports.py
@@ -16,23 +16,6 @@ assert op.suffix == ".csv"
 # read long CSV
 df = pd.read_csv(ip)
 
-# workaround until intel compilers are installable on ubuntu-20.04 via setup-fortran
-ub20_ifort = df[
-    (df["compiler"] == "intel-classic") & (df["runner"] == "macos-12")
-].copy()
-ub20_ifort["runner"] = "ubuntu-20.04"
-ub20_ifort["support"] = "?"
-ub20_ifort[
-    (ub20_ifort["runner"] == "ubuntu-20.04") & (ub20_ifort["version"] == 2021.7)
-]["support"] = "&check;"
-
-ub20_ifx = df[(df["compiler"] == "intel") & (df["runner"] == "ubuntu-22.04")].copy()
-ub20_ifx["runner"] = "ubuntu-20.04"
-ub20_ifx["support"] = "?"
-
-
-df = pd.concat([df, ub20_ifort, ub20_ifx])
-
 # pivot and sort
 df = pd.pivot(
     df,
