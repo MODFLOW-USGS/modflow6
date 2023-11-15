37bb1c9b9c154521e776b9fa31c1a2ab499cbc26
wpbonelli <wbonelli@ucar.edu>
2023-11-15 09:43:36 -0500

    test: add minimal unit tests for some utilities (#1409)

--- autotest/tester.f90
+++ autotest/tester.f90
@@ -3,7 +3,10 @@ program tester
   use testdrive, only: run_testsuite, new_testsuite, testsuite_type, &
     & select_suite, run_selected, get_argument
   use TestArrayHandlers, only: collect_arrayhandlers
+  use TestDevFeature, only: collect_dev_feature
   use TestGeomUtil, only: collect_geomutil
+  use TestInputOutput, only: collect_inputoutput
+  use TestSim, only: collect_sim
   implicit none
   integer :: stat, is
   character(len=:), allocatable :: suite_name, test_name
@@ -13,7 +16,10 @@ program tester
   stat = 0
   testsuites = [ &
                new_testsuite("ArrayHandlers", collect_arrayhandlers), &
-               new_testsuite("GeomUtil", collect_geomutil) &
+               new_testsuite("DevFeature", collect_dev_feature), &
+               new_testsuite("GeomUtil", collect_geomutil), &
+               new_testsuite("InputOutput", collect_inputoutput), &
+               new_testsuite("Sim", collect_sim) &
                ]
 
   call get_argument(1, suite_name)
