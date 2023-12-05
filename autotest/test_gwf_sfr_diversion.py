61b502742e87906ba8ba9b5229d028534801e13f
wpbonelli <wbonelli@ucar.edu>
2023-12-05 10:35:29 -0500

    update test_gwf_sfr_diversion.py

--- autotest/test_gwf_sfr_diversion.py
+++ autotest/test_gwf_sfr_diversion.py
@@ -1,20 +1,22 @@
 import os
+
 import flopy
 import numpy as np
 import pytest
 from framework import TestFramework
-from simulation import TestSimulation
 
 ex = ["sfr_div"]
-inflows   = np.array([10,  0, 10,  0, 10])
-diversion = np.array([.5, .5, .5, .5, .0])
+inflows = np.array([10, 0, 10, 0, 10])
+diversion = np.array([0.5, 0.5, 0.5, 0.5, 0.0])
 
-def build_model(idx, ws, ):
 
+def build_models(idx, test):
     # static model data
     # temporal discretization
     nper = len(inflows)
-    tdis_rc = [(1.0, 1, 1.0),] * nper
+    tdis_rc = [
+        (1.0, 1, 1.0),
+    ] * nper
 
     # spatial discretization data
     nlay, nrow, ncol = 1, 1, 1
@@ -29,10 +31,10 @@ def build_model(idx, ws, ):
         sim_name=name,
         version="mf6",
         exe_name="mf6",
-        sim_ws=ws,
+        sim_ws=test.workspace,
     )
     sim.simulation_data.verify_data = False
-    
+
     # create tdis package
     tdis = flopy.mf6.ModflowTdis(
         sim,
@@ -68,15 +70,18 @@ def build_model(idx, ws, ):
     ic = flopy.mf6.ModflowGwfic(gwf, strt=strt)
 
     # node property flow
-    npf = flopy.mf6.ModflowGwfnpf(gwf, icelltype=0, )
+    npf = flopy.mf6.ModflowGwfnpf(
+        gwf,
+        icelltype=0,
+    )
 
     # output control
-    oc = flopy.mf6.ModflowGwfoc(gwf, 
-                                budget_filerecord=name+'.cbb', 
-                                saverecord=[['BUDGET', 'ALL']])
+    oc = flopy.mf6.ModflowGwfoc(
+        gwf, budget_filerecord=name + ".cbb", saverecord=[["BUDGET", "ALL"]]
+    )
 
     # sfr file
-    cellid = (0,0,0)
+    cellid = (0, 0, 0)
     nreaches = 3
     rlen = 10.0
     rwid = 10.0
@@ -84,12 +89,24 @@ def build_model(idx, ws, ):
     rbth = 1.0
     rhk = 0.0
     slope = 0.001
-    
+
     sfrrch_prop = [cellid, rlen, rwid, slope, top, rbth, rhk, roughness]
     packagedata = [
-        [0,] + sfrrch_prop + [2, 1.0, 1],
-        [1,] + sfrrch_prop + [1, 0.0, 0],
-        [2,] + sfrrch_prop + [1, 1.0, 0],
+        [
+            0,
+        ]
+        + sfrrch_prop
+        + [2, 1.0, 1],
+        [
+            1,
+        ]
+        + sfrrch_prop
+        + [1, 0.0, 0],
+        [
+            2,
+        ]
+        + sfrrch_prop
+        + [1, 1.0, 0],
     ]
     connectiondata = [
         [0, -1, -2],
@@ -97,20 +114,20 @@ def build_model(idx, ws, ):
         [2, 0],
     ]
     diversiondata = [
-        [0, 0, 1, 'FRACTION'],
+        [0, 0, 1, "FRACTION"],
     ]
     perioddata = {
-        i: [[0, "inflow", qin], 
-            [0, "diversion", 0, qdiv]]
-        for i, (qin, qdiv) in enumerate(zip(inflows, diversion))}
+        i: [[0, "inflow", qin], [0, "diversion", 0, qdiv]]
+        for i, (qin, qdiv) in enumerate(zip(inflows, diversion))
+    }
 
     sfr = flopy.mf6.ModflowGwfsfr(
         gwf,
         print_stage=True,
         print_flows=True,
         print_input=True,
-        budgetcsv_filerecord=name+'.sfr.csv',
-        budget_filerecord=name+'.sfr.cbb',
+        budgetcsv_filerecord=name + ".sfr.csv",
+        budget_filerecord=name + ".sfr.cbb",
         nreaches=nreaches,
         packagedata=packagedata,
         connectiondata=connectiondata,
@@ -122,46 +139,42 @@ def build_model(idx, ws, ):
     return sim, None
 
 
-def eval_results(sim):
+def check_output(test):
     print("evaluating results...")
 
     # check flow for indivdual reach
-    fname = os.path.join(sim.simpath, sim.name+'.sfr.cbb')
+    fname = os.path.join(test.workspace, f"{test.name}.sfr.cbb")
     with flopy.utils.CellBudgetFile(fname) as cbb:
-        outflows = cbb.get_data(text='EXT-OUTFLOW')
-    
+        outflows = cbb.get_data(text="EXT-OUTFLOW")
+
     # check outflow for reach 2 and 3
-    assert np.allclose([r.q[1] for r in outflows], -inflows*diversion), \
-           'Incorrect outflow for diversion reach'
-    assert np.allclose([r.q[2] for r in outflows], -inflows*(1-diversion)), \
-           'Incorrect outflow for outlet reach'
-    
+    assert np.allclose(
+        [r.q[1] for r in outflows], -inflows * diversion
+    ), "Incorrect outflow for diversion reach"
+    assert np.allclose(
+        [r.q[2] for r in outflows], -inflows * (1 - diversion)
+    ), "Incorrect outflow for outlet reach"
+
     # load SFR budget CSV and check overall budget
-    with open(fname.replace('.cbb', '.csv')) as f:
-        header = f.readline().strip().split(',')
-        flux = np.loadtxt(f, delimiter=',')
-        
-    assert np.allclose(flux[:, header.index('EXT-OUTFLOW_IN')], 0), \
-           'External flow IN larger than zero'
-    assert np.allclose(flux[:, header.index('PERCENT_DIFFERENCE')], 0), \
-           'Large mass balance error in SFR'
+    with open(fname.replace(".cbb", ".csv")) as f:
+        header = f.readline().strip().split(",")
+        flux = np.loadtxt(f, delimiter=",")
+
+    assert np.allclose(
+        flux[:, header.index("EXT-OUTFLOW_IN")], 0
+    ), "External flow IN larger than zero"
+    assert np.allclose(
+        flux[:, header.index("PERCENT_DIFFERENCE")], 0
+    ), "Large mass balance error in SFR"
 
 
-@pytest.mark.parametrize(
-    "idx, name",
-    list(enumerate(ex)),
-)
+@pytest.mark.parametrize("idx, name", list(enumerate(ex)))
 def test_mf6model(idx, name, function_tmpdir, targets):
-    ws = str(function_tmpdir)
-    test = TestFramework()
-    test.build(build_model, idx, ws)
-    test.run(
-        TestSimulation(
-            name=name, 
-            exe_dict=targets, 
-            exfunc=eval_results, 
-            idxsim=idx
-        ),
-        ws,
+    test = TestFramework(
+        name=name,
+        workspace=function_tmpdir,
+        targets=targets,
+        build=lambda t: build_models(idx, t),
+        check=check_output,
     )
-
+    test.run()
