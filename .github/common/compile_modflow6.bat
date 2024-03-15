96c915e47a8723824c2aec48ab81d0ca0cb789bc
wpbonelli <wbonelli@ucar.edu>
2024-03-15 14:22:10 -0400

    serial meson test

--- .github/common/compile_modflow6.bat
+++ .github/common/compile_modflow6.bat
@@ -2,5 +2,4 @@ set FC=ifort
 cd "%GITHUB_WORKSPACE%\modflow6"
 micromamba run -r "C:\Users\runneradmin\micromamba" -n modflow6 meson setup builddir -Ddebug=false -Dparallel=true --prefix=%CD% --libdir=bin
 micromamba run -r "C:\Users\runneradmin\micromamba" -n modflow6 meson install -C builddir
-:: commented until flakiness is resolved
-:: micromamba run -r "C:\Users\runneradmin\micromamba" -n modflow6 meson test --verbose --no-rebuild -C builddir
+micromamba run -r "C:\Users\runneradmin\micromamba" -n modflow6 meson test --verbose --no-rebuild -C builddir
