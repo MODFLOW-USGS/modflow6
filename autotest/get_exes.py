MODFLOW-USGS/modflow6

commit 0e228cfc849411248b584cd98e6f122a7d899c42

ci(release): support model selection for docs/dfns/examples (#1746)
* select model types to include in docs/dfns/examples at release time
* don't rebuild mf6io.pdf in build_documentation() if it already exists
* don't build examples in dist build job (not needed)
* last step toward feature toggling

.github/workflows/release.yml
@@ -47,6 +47,10 @@ on:
         description: 'Version number to use for release.'
         required: true
         type: string
+      models:
+        description: 'Models to include in the release documentation'
+        required: false
+        type: string
     outputs:
       version:
         description: 'Version number used for release'
@@ -419,7 +423,9 @@ jobs:
       - name: Build example models
         if: inputs.full == true
         working-directory: modflow6-examples/autotest
-        run: pytest -v -n auto test_scripts.py --init
+        run: |
+          models="${{ inputs.models }}"
+          pytest -v -n auto test_scripts.py --init -k "${models//,/ or }"
       
       - name: Create folder structure
         if: inputs.full == true
@@ -461,7 +467,8 @@ jobs:
           GITHUB_TOKEN: ${{ github.token }}
         run: |
           mkdir -p "${{ needs.build.outputs.distname }}/doc"
-          cmd="python modflow6/distribution/build_docs.py -b bin -o doc"
+          models="${{ inputs.models }}"
+          cmd="python modflow6/distribution/build_docs.py -b bin -o doc -m ${models//,/ -m }"
           if [[ "${{ inputs.full }}" == "true" ]]; then
             cmd="$cmd --full"
           fi
@@ -568,40 +575,14 @@ jobs:
           echo "dist directory contains:"
           ls
       
-      - name: Install dependencies for example models
-        env:
-          GITHUB_TOKEN: ${{ github.token }}
-        run: |
-          pip install -r modflow6-examples/etc/requirements.pip.txt
-          distname="${{ needs.build.outputs.distname }}_${{ steps.ostag.outputs.ostag }}"
-          echo "$distname/bin" >> $GITHUB_PATH
-          # execute permissions may not have survived artifact upload/download
-          chmod +x "$distname/bin/mf6"
-          chmod +x "$distname/bin/mf5to6"
-          chmod +x "$distname/bin/zbud6"
-
-      # the example models also need mf2005, triangle and gridgen
-      - name: Install extra executables
-        uses: modflowpy/install-modflow-action@v1
-        with:
-          subset: mf2005,triangle,gridgen
-
-      - name: Update Flopy
-        working-directory: modflow6/autotest
-        run: python update_flopy.py
-  
-      - name: Build example models
-        if: inputs.full == true
-        working-directory: modflow6-examples/autotest
-        run: pytest -v -n auto test_scripts.py --init
-
       - name: Build distribution
         env:
           GITHUB_TOKEN: ${{ github.token }}
         run: |
           # build distribution
           distname="${{ needs.build.outputs.distname }}_${{ steps.ostag.outputs.ostag }}"
-          cmd="python modflow6/distribution/build_dist.py -o $distname -e modflow6-examples"
+          models="${{ inputs.models }}"
+          cmd="python modflow6/distribution/build_dist.py -o $distname -m ${models//,/ -m }"
           if [[ "${{ inputs.full }}" == "true" ]]; then
             cmd="$cmd --full"
           fi

distribution/build_dist.py
@@ -7,25 +7,26 @@ from os import PathLike, environ
 from pathlib import Path
 from pprint import pprint
 from shutil import copy, copyfile, copytree, ignore_patterns
+from typing import List, Optional
 
 import pytest
-from modflow_devtools.build import meson_build
-from modflow_devtools.download import download_and_unzip, get_release
-from modflow_devtools.markers import no_parallel, requires_exe
-from modflow_devtools.misc import get_model_paths
-
 from build_docs import build_documentation
 from build_makefiles import (
     build_mf5to6_makefile,
     build_mf6_makefile,
     build_zbud6_makefile,
 )
-from utils import get_project_root_path, run_command
+from modflow_devtools.build import meson_build
+from modflow_devtools.download import download_and_unzip, get_release
+from modflow_devtools.markers import no_parallel, requires_exe
+from modflow_devtools.misc import get_model_paths
+
+from utils import get_project_root_path
 
 # default paths
 _project_root_path = get_project_root_path()
-_examples_repo_path = _project_root_path.parent / "modflow6-examples"
 _build_path = _project_root_path / "builddir"
+_default_models = ["gwf", "gwt", "gwe", "prt", "swf"]
 
 # OS-specific extensions
 _system = platform.system()
@@ -107,7 +108,10 @@ def test_copy_sources(tmp_path):
 
 
 def setup_examples(
-    bin_path: PathLike, examples_path: PathLike, overwrite: bool = False
+    bin_path: PathLike,
+    examples_path: PathLike,
+    overwrite: bool = False,
+    models: Optional[List[str]] = None,
 ):
     examples_path = Path(examples_path).expanduser().absolute()
 
@@ -118,6 +122,11 @@ def setup_examples(
         iter([a for a in assets if a["name"] == "modflow6-examples.zip"]), None
     )
     download_and_unzip(asset["browser_download_url"], examples_path, verbose=True)
+
+    # filter examples for models selected for release
+    for p in examples_path.glob("*"):
+        if not any(m in p.stem for m in models):
+            p.unlink()
 
     # list folders with mfsim.nam (recursively)
     # and add run.sh/bat script to each folder
@@ -266,15 +275,14 @@ def test_build_makefiles(tmp_path):
 def build_distribution(
     build_path: PathLike,
     output_path: PathLike,
-    examples_repo_path: PathLike,
     full: bool = False,
     overwrite: bool = False,
+    models: Optional[List[str]] = None,
 ):
     print(f"Building {'full' if full else 'minimal'} distribution")
 
     build_path = Path(build_path).expanduser().absolute()
     output_path = Path(output_path).expanduser().absolute()
-    examples_repo_path = Path(examples_repo_path).expanduser().absolute()
 
     # binaries
     build_programs_meson(
@@ -288,11 +296,12 @@ def build_distribution(
     if not full:
         return
 
-    # examples
+    # download and setup example models
     setup_examples(
         bin_path=output_path / "bin",
         examples_path=output_path / "examples",
         overwrite=overwrite,
+        models=models,
     )
 
     # copy source code files
@@ -301,7 +310,7 @@ def build_distribution(
     # build and copy makefiles
     build_makefiles(output_path=output_path)
 
-    # docs
+    # build docs
     build_documentation(
         bin_path=output_path / "bin",
         full=full,
@@ -319,7 +328,6 @@ def test_build_distribution(tmp_path, full):
     build_distribution(
         build_path=tmp_path / "builddir",
         output_path=output_path,
-        examples_repo_path=_examples_repo_path,
         full=full,
         overwrite=True,
     )
@@ -378,11 +386,11 @@ if __name__ == "__main__":
         help="Path to create distribution artifacts",
     )
     parser.add_argument(
-        "-e",
-        "--examples-repo-path",
+        "-m",
+        "--model",
         required=False,
-        default=str(_examples_repo_path),
-        help="Path to directory containing modflow6 example models",
+        action="append",
+        help="Filter models to include",
     )
     parser.add_argument(
         "--full",
@@ -402,20 +410,13 @@ if __name__ == "__main__":
     args = parser.parse_args()
     build_path = Path(args.build_path)
     out_path = Path(args.output_path)
-    examples_repo_path = (
-        Path(args.examples_repo_path)
-        if args.examples_repo_path
-        else _examples_repo_path
-    )
-    assert (
-        examples_repo_path.is_dir()
-    ), f"Examples repo not found at path: {examples_repo_path}"
     out_path.mkdir(parents=True, exist_ok=True)
+    models = args.model if args.model else _default_models
 
     build_distribution(
         build_path=build_path,
         output_path=out_path,
-        examples_repo_path=examples_repo_path,
         full=args.full,
         overwrite=args.force,
+        models=models,
     )

.github/workflows/release_dispatch.yml
@@ -48,6 +48,10 @@ on:
         description: 'Version number to use for release.'
         required: true
         type: string
+      models:
+        description: 'Models to include in the release documentation'
+        required: false
+        type: string
 jobs:
   set_options:
     name: Set release options
@@ -137,6 +141,7 @@ jobs:
       full: true
       run_tests: ${{ inputs.run_tests }}
       version: ${{ needs.set_options.outputs.version }}
+      models: ${{ inputs.models }}
   pr:
     name: Draft release PR
     if: ${{ github.ref_name != 'master' && ((github.event_name == 'workflow_dispatch' && inputs.approve == 'true') || (github.event_name != 'workflow_dispatch' && !contains(github.ref_name, 'rc'))) }}

distribution/build_docs.py
@@ -14,6 +14,7 @@ from urllib.error import HTTPError
 from warnings import warn
 
 import pytest
+from benchmark import run_benchmarks
 from flaky import flaky
 from modflow_devtools.build import meson_build
 from modflow_devtools.download import (
@@ -25,7 +26,6 @@ from modflow_devtools.download import (
 from modflow_devtools.markers import no_parallel, requires_exe, requires_github
 from modflow_devtools.misc import is_in_ci, run_cmd, set_dir
 
-from benchmark import run_benchmarks
 from utils import convert_line_endings, get_project_root_path
 
 # paths
@@ -47,6 +47,7 @@ _full_dist_tex_paths = [
     _docs_path / "ConverterGuide" / "converter_mf5to6.tex",
     _docs_path / "SuppTechInfo" / "mf6suptechinfo.tex",
 ]
+_default_models = ["gwf", "gwt", "gwe", "prt", "swf"]
 
 # OS-specific extensions
 _system = platform.system()
@@ -219,7 +220,9 @@ def build_deprecations_tex():
     assert (_docs_path / "ReleaseNotes" / f"{deprecations_path.stem}.tex").is_file()
 
 
-def build_mf6io_tex_from_dfn(overwrite: bool = False):
+def build_mf6io_tex_from_dfn(
+    overwrite: bool = False, models: Optional[List[str]] = None
+):
     if overwrite:
         clean_tex_files()
 
@@ -261,7 +264,11 @@ def build_mf6io_tex_from_dfn(overwrite: bool = False):
                 f.unlink()
 
             # run python script
-            out, err, ret = run_cmd(sys.executable, "mf6ivar.py")
+            args = [sys.executable, "mf6ivar.py"]
+            if models is not None and any(models):
+                for model in models:
+                    args += ["--model", model]
+            out, err, ret = run_cmd(*args)
             assert not ret, out + err
 
             # check that dfn and tex files match
@@ -473,17 +480,22 @@ def build_documentation(
     output_path: Optional[PathLike] = None,
     overwrite: bool = False,
     repo_owner: str = "MODFLOW-USGS",
+    models: Optional[List[str]] = None,
 ):
     print(f"Building {'full' if full else 'minimal'} documentation")
 
     bin_path = Path(bin_path).expanduser().absolute()
     output_path = Path(output_path).expanduser().absolute()
 
+    if (output_path / "mf6io.pdf").is_file() and not overwrite:
+        print(f"{output_path / 'mf6io.pdf'} already exists")
+        return
+
     # make sure output directory exists
     output_path.mkdir(parents=True, exist_ok=True)
 
     # build LaTex input/output docs from DFN files
-    build_mf6io_tex_from_dfn(overwrite=True)
+    build_mf6io_tex_from_dfn(overwrite=overwrite, models=models)
 
     # build LaTeX input/output example model docs
     with TemporaryDirectory() as temp:
@@ -608,14 +620,23 @@ if __name__ == "__main__":
         default="MODFLOW-USGS",
         help="Repository owner (substitute your own for a fork)",
     )
+    parser.add_argument(
+        "-m",
+        "--model",
+        required=False,
+        action="append",
+        help="Filter model types to include",
+    )
     args = parser.parse_args()
     output_path = Path(args.output_path).expanduser().absolute()
     output_path.mkdir(parents=True, exist_ok=True)
     bin_path = Path(args.bin_path).expanduser().absolute()
+    models = args.model if args.model else _default_models
     build_documentation(
         bin_path=bin_path,
         full=args.full,
         output_path=output_path,
         overwrite=args.force,
         repo_owner=args.repo_owner,
+        models=models,
     )

commit cea54b2a6b6809ef0b7420035aa863d03350294f

ci(release): fix model selection options for dist scripts (#1752)
* fix nightly build broken by #1746
* fix model selection, can't assume models provided
* fix example filtering in build_dist.py, use rmtree not unlink
* unrelated: remove old workaround from autotest/get_exes.py

.github/workflows/release.yml
@@ -467,8 +467,11 @@ jobs:
           GITHUB_TOKEN: ${{ github.token }}
         run: |
           mkdir -p "${{ needs.build.outputs.distname }}/doc"
+          cmd="python modflow6/distribution/build_docs.py -b bin -o doc"
           models="${{ inputs.models }}"
-          cmd="python modflow6/distribution/build_docs.py -b bin -o doc -m ${models//,/ -m }"
+          if [ -n "$models" ]; then
+            cmd="$cmd -m ${models//,/ -m }"
+          fi
           if [[ "${{ inputs.full }}" == "true" ]]; then
             cmd="$cmd --full"
           fi
@@ -581,8 +584,11 @@ jobs:
         run: |
           # build distribution
           distname="${{ needs.build.outputs.distname }}_${{ steps.ostag.outputs.ostag }}"
+          cmd="python modflow6/distribution/build_dist.py -o $distname"
           models="${{ inputs.models }}"
-          cmd="python modflow6/distribution/build_dist.py -o $distname -m ${models//,/ -m }"
+          if [ -n "$models" ]; then
+            cmd="$cmd -m ${models//,/ -m }"
+          fi
           if [[ "${{ inputs.full }}" == "true" ]]; then
             cmd="$cmd --full"
           fi

autotest/get_exes.py
@@ -53,6 +53,7 @@ def test_rebuild_release(rebuilt_bin_path: Path):
         )
 
     with TemporaryDirectory() as td:
+        # download the release
         download_path = Path(td)
         download_and_unzip(
             asset["browser_download_url"],
@@ -76,21 +77,11 @@ def test_rebuild_release(rebuilt_bin_path: Path):
                 f.write(f"{line}\n")
 
         # rebuild with Meson
-        def rebuild():
-            meson_build(
-                project_path=source_files_path.parent,
-                build_path=download_path / "builddir",
-                bin_path=rebuilt_bin_path,
-            )
-
-        # temp workaround until next release,
-        # ifx fails to build 6.4.2 on Windows
-        # most likely due to backspace issues
-        if system() == "Windows" and environ.get("FC") == "ifx":
-            with set_env(FC="ifort", CC="icl"):
-                rebuild()
-        else:
-            rebuild()
+        meson_build(
+            project_path=source_files_path.parent,
+            build_path=download_path / "builddir",
+            bin_path=rebuilt_bin_path,
+        )
 
 
 @flaky(max_runs=3)

distribution/build_dist.py
@@ -6,7 +6,7 @@ import textwrap
 from os import PathLike, environ
 from pathlib import Path
 from pprint import pprint
-from shutil import copy, copyfile, copytree, ignore_patterns
+from shutil import copy, copyfile, copytree, ignore_patterns, rmtree
 from typing import List, Optional
 
 import pytest
@@ -114,19 +114,18 @@ def setup_examples(
     models: Optional[List[str]] = None,
 ):
     examples_path = Path(examples_path).expanduser().absolute()
-
-    # download example models zip asset
     latest = get_release("MODFLOW-USGS/modflow6-examples", "latest")
     assets = latest["assets"]
     asset = next(
         iter([a for a in assets if a["name"] == "modflow6-examples.zip"]), None
     )
+    # download example models zip asset
     download_and_unzip(asset["browser_download_url"], examples_path, verbose=True)
 
     # filter examples for models selected for release
     for p in examples_path.glob("*"):
         if not any(m in p.stem for m in models):
-            p.unlink()
+            rmtree(p)
 
     # list folders with mfsim.nam (recursively)
     # and add run.sh/bat script to each folder