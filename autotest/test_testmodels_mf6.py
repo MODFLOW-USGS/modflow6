9c0dbef4e95df4d2a6c5343d210d0f0d9547e95a
wpbonelli <wbonelli@ucar.edu>
2024-05-06 14:17:12 -0400

    test(modflow6-testmodels): reinstate excluded models (#1767)
    * continue excluding test014_NWTP3Low_dev until convergence failure resolved

--- autotest/test_testmodels_mf6.py
+++ autotest/test_testmodels_mf6.py
@@ -1,25 +1,19 @@
 from shutil import copytree
 
 import pytest
-
-from framework import TestFramework
 from common_regression import (
+    get_mf6_comparison,
     setup_mf6,
     setup_mf6_comparison,
-    get_mf6_comparison,
 )
+from framework import TestFramework
 
 excluded_models = [
     "alt_model",
     "test205_gwtbuy-henrytidal",
-    # todo reinstate when flopy fixed: https://github.com/modflowpy/flopy/issues/2053
-    "test001a_Tharmonic_tabs",
-    "test004_bcfss",
-    "test014_NWTP3Low_dev",
-    "test041_flowdivert_nwt_dev",
     # todo reinstate after 6.5.0 release
     "test001d_Tnewton",
-    # remove tests with nwt usg conductance weighting 
+    # remove tests with nwt usg conductance weighting
     "test006_gwf3_gnc_nr_dev",
     "test006_gwf3_nr_dev",
     "test014_NWTP3High_dev",
@@ -28,9 +22,8 @@ excluded_models = [
     "test016_Keating_disu_dev",
     "test053_npf-a-nwt_dev",
     "test053_npf-b-nwt_dev",
-    # todo reinstate when flopy fixed: https://github.com/modflowpy/flopy/issues/2145
-    "test001h_evt_array4",
-    "test001h_rch_array4",
+    # todo reinstate after resolving convergence failure
+    "test014_NWTP3Low_dev"
 ]
 
 
@@ -47,7 +40,11 @@ def test_model(
     model_path = test_model_mf6.parent
     model_name = model_path.name
     excluded = model_name in excluded_models
-    compare = get_mf6_comparison(model_path) if original_regression else "mf6_regression"
+    compare = (
+        get_mf6_comparison(model_path)
+        if original_regression
+        else "mf6_regression"
+    )
     dev_only = "dev" in model_name and "not developmode" in markers
     if excluded or dev_only:
         reason = "excluded" if excluded else "developmode only"
