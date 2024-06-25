6f5c2f631e30479be0b15117ac58689adad0e089
Joseph Hughes <jdhughes@usgs.gov>
2024-06-25 17:02:38 -0500

    * update

--- src/Model/GroundWaterFlow/gwf-sfr.f90
+++ src/Model/GroundWaterFlow/gwf-sfr.f90
@@ -235,6 +235,55 @@ module SfrModule
     procedure :: sfr_activate_viscosity
   end type SfrType
 
+  interface
+    module subroutine sfr_calc_steady(this, n, d1, hgwf, &
+                                      qu, qi, qfrommvr, qr, qe, qro, &
+                                      qgwf, qd)
+      class(SfrType) :: this !< SfrType object
+      integer(I4B), intent(in) :: n !< reach number
+      real(DP), intent(inout) :: d1 !< current reach depth estimate
+      real(DP), intent(in) :: hgwf !< head in gw cell
+      real(DP), intent(in) :: qu !< reach upstream flow
+      real(DP), intent(in) :: qi !< reach specified inflow
+      real(DP), intent(in) :: qfrommvr !< reach flow from mover
+      real(DP), intent(in) :: qr !< reach rainfall
+      real(DP), intent(in) :: qe !< reach evaporation
+      real(DP), intent(in) :: qro !< reach runoff flow
+      real(DP), intent(inout) :: qgwf !< reach-aquifer exchange
+      real(DP), intent(inout) :: qd !< reach outflow
+    end subroutine
+  end interface
+
+  interface
+    module subroutine sfr_calc_transient(this, n, d1, hgwf, &
+                                         qu, qi, qfrommvr, qr, qe, qro, &
+                                         qgwf, qd)
+      class(SfrType) :: this !< SfrType object
+      integer(I4B), intent(in) :: n !< reach number
+      real(DP), intent(inout) :: d1 !< current reach depth estimate
+      real(DP), intent(in) :: hgwf !< head in gw cell
+      real(DP), intent(in) :: qu !< reach upstream flow
+      real(DP), intent(in) :: qi !< reach specified inflow
+      real(DP), intent(in) :: qfrommvr !< reach flow from mover
+      real(DP), intent(in) :: qr !< reach rainfall
+      real(DP), intent(in) :: qe !< reach evaporation
+      real(DP), intent(in) :: qro !< reach runoff flow
+      real(DP), intent(inout) :: qgwf !< reach-aquifer exchange
+      real(DP), intent(inout) :: qd !< reach outflow
+    end subroutine
+  end interface
+
+  interface
+    module subroutine sfr_calc_constant(this, n, d1, hgwf, qgwf, qd)
+      class(SfrType) :: this !< SfrType object
+      integer(I4B), intent(in) :: n !< reach number
+      real(DP), intent(inout) :: d1 !< current reach depth estimate
+      real(DP), intent(in) :: hgwf !< head in gw cell
+      real(DP), intent(inout) :: qgwf !< reach-aquifer exchange
+      real(DP), intent(inout) :: qd !< reach outflow
+    end subroutine
+  end interface  
+
 contains
 
   !> @ brief Create a new package object
@@ -4145,186 +4194,6 @@ contains
     ! -- return
     return
   end subroutine sfr_calc_cond
-
-  subroutine sfr_calc_transient(this, n, d1, hgwf, &
-                                qu, qi, qfrommvr, qr, qe, qro, &
-                                qgwf, qd)
-    use TdisModule, only: delt
-    ! -- dummy variables
-    class(SfrType) :: this !< SfrType object
-    integer(I4B), intent(in) :: n !< reach number
-    real(DP), intent(inout) :: d1 !< current reach depth estimate
-    real(DP), intent(in) :: hgwf !< head in gw cell
-    real(DP), intent(in) :: qu !< reach upstream flow
-    real(DP), intent(in) :: qi !< reach specified inflow
-    real(DP), intent(in) :: qfrommvr !< reach flow from mover
-    real(DP), intent(in) :: qr !< reach rainfall
-    real(DP), intent(in) :: qe !< reach evaporation
-    real(DP), intent(in) :: qro !< reach runoff flow
-    real(DP), intent(inout) :: qgwf !< reach-aquifer exchange
-    real(DP), intent(inout) :: qd !< reach outflow
-    ! -- local  variables
-    integer(I4B) :: igwfconn
-    integer(I4B) :: number_picard
-    integer(I4B) :: iconverged
-    integer(I4B) :: i
-    integer(I4B) :: j
-    real(DP) :: kinematic_residual
-    real(DP) :: kinematic_storage
-    real(DP) :: weight
-    ! real(DP) :: weightinv
-    real(DP) :: dq
-    real(DP) :: qtol
-    real(DP) :: qsrc
-    real(DP) :: qlat
-    real(DP) :: da
-    real(DP) :: db
-    real(DP) :: dc
-    real(DP) :: dd
-    real(DP) :: qa
-    real(DP) :: qb
-    real(DP) :: qc
-    real(DP) :: xsa_a
-    real(DP) :: xsa_b
-    real(DP) :: xsa_c
-    ! real(DP) :: xsa_d
-    real(DP) :: d1old
-    real(DP) :: qd2
-    real(DP) :: ad
-    real(DP) :: ad2
-    real(DP) :: qgwf_pul
-    ! real(DP) :: f11
-    ! real(DP) :: f12
-    real(DP) :: residual
-    real(DP) :: residual2
-    real(DP) :: residual_final
-    real(DP) :: qderv
-    real(DP) :: delq
-    real(DP) :: delh
-    ! real(DP) :: delhold
-    ! real(DP) :: ibisect_count
-    real(DP) :: dd2
-    ! real(DP) :: darea
-
-    ! -- initialize local parameters
-    weight = this%storage_weight
-    ! weightinv = DONE - weight
-    dq = this%deps !DEM6 !DEM4 !this%deps
-    qtol = dq * DTWO !1e-9 !dq * DTWO
-
-    ! -- initialize variables
-    qgwf = DZERO
-    qgwf_pul = DZERO
-
-    !
-    ! -- calculate the flow at end of the reach
-    !    excluding groundwater leakage
-    qsrc = qu + qi + qr - qe + qro + qfrommvr
-
-    qlat = (qr + qro - qe) / this%length(n)
-
-    this%usinflow(n) = qu + qi + qfrommvr
-
-    qa = this%usinflowold(n)
-    qb = this%dsflowold(n)
-    qc = this%usinflow(n)
-    call this%sfr_calc_reach_depth(n, qa, da)
-    call this%sfr_calc_reach_depth(n, qb, db)
-    call this%sfr_calc_reach_depth(n, qc, dc)
-
-    xsa_a = this%calc_area_wet(n, da)
-    xsa_b = this%calc_area_wet(n, db)
-    xsa_c = this%calc_area_wet(n, dc)
-
-    ! -- estimate qd
-    qd = (qc + qb) * DHALF
-    call this%sfr_calc_reach_depth(n, qd, dd)
-    ad = this%calc_area_wet(n, dd)
-
-    ! -- estimate the depth at the midpoint
-    d1 = (dc + dd) * DHALF
-    d1old = d1
-
-    igwfconn = this%sfr_gwf_conn(n)
-    number_picard = this%maxsfrpicard
-    if (igwfconn == 1) then
-      number_picard = this%maxsfrpicard
-    else
-      number_picard = 1
-    end if
-
-    kinematicpicard: do i = 1, number_picard
-      if (igwfconn == 1) then
-        call this%sfr_calc_qgwf(n, d1, hgwf, qgwf)
-        if (qgwf > qsrc) then
-          qgwf = qsrc
-        end if
-      end if
-
-      qsrc = qlat - qgwf / this%length(n)
-
-      newton: do j = 1, this%maxsfrit
-        qd2 = qd + dq
-        call this%sfr_calc_reach_depth(n, qd2, dd2)
-        ad2 = this%calc_area_wet(n, dd2)
-
-        residual = kinematic_residual(qa, qb, qc, qd, &
-                                      xsa_a, xsa_b, xsa_c, ad, &
-                                      qsrc, this%length(n), weight, delt)
-
-        residual2 = kinematic_residual(qa, qb, qc, qd2, &
-                                       xsa_a, xsa_b, xsa_c, ad2, &
-                                       qsrc, this%length(n), weight, delt)
-        qderv = (residual2 - residual) / dq
-        if (qderv > DZERO) then
-          delq = -residual / qderv
-        else
-          delq = DZERO
-        end if
-
-        if (qd + delq < DEM30) then
-          delq = -qd
-        end if
-
-        qd = qd + delq
-
-        call this%sfr_calc_reach_depth(n, qd, dd)
-        ad = this%calc_area_wet(n, dd)
-        residual_final = kinematic_residual(qa, qb, qc, qd, &
-                                            xsa_a, xsa_b, xsa_c, ad, &
-                                            qsrc, this%length(n), weight, delt)
-
-        if (abs(delq) < qtol) then ! .and. abs(residual_final) < qtol) then
-          exit newton
-        end if
-
-      end do newton
-
-      qd = max(qd, DZERO)
-      d1 = (dc + dd) * DHALF
-      ! if (qd == DZERO) then
-      !   d1 = DZERO
-      ! else
-      !   d1 = (dc + dd) * DHALF
-      ! end if
-      delh = (d1 - d1old)
-
-      iconverged = 0
-      if (i == 1 .and. qd == DZERO) then
-        iconverged = 1
-      end if
-      if (i > 1 .and. abs(delh) < this%dmaxchg) then
-        iconverged = 1
-      end if
-      if (iconverged == 1) then
-        exit kinematicpicard
-      end if
-
-    end do kinematicpicard
-
-    this%storage(n) = kinematic_storage(qc, qd)
-
-  end subroutine sfr_calc_transient
 
   !> @brief Calculate diversion flow
     !!
