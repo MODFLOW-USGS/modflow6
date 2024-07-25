d46581e989890c35ffd4ae1443af9d61faeed29a
wpbonelli <wbonelli@ucar.edu>
2024-07-25 14:43:49 -0400

    check testmodels, renaming, run serial tests with xdist

--- .github/common/test_modflow6_serial.bat
+++ .github/common/test_modflow6_serial.bat
@@ -1,4 +1,4 @@
 cd "%GITHUB_WORKSPACE%\modflow6\autotest"
 where libpetsc.dll
 ldd ..\bin\mf6
-pixi run pytest -v --durations=0 --keep-failed .failed -m "%MARKERS%"
+pixi run autotest -m "%MARKERS%"
