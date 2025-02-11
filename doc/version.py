14ec3a5552ef57efdba77b8d516f2b1d8a5ea279
wpbonelli <wbonelli@contractor.usgs.gov>
2025-02-10 20:20:13 -0500

    ci(release): update version to 6.7.0.dev1

--- doc/version.py
+++ doc/version.py
@@ -1,3 +1,3 @@
 # MODFLOW 6 version file automatically created using...update_version.py
-# created on...February 10, 2025 20:50:33
-__version__ = "6.6.1"
+# created on...February 10, 2025 20:19:09
+__version__ = "6.7.0.dev1"
--- src/Utilities/version.f90
+++ src/Utilities/version.f90
@@ -17,8 +17,8 @@ module VersionModule
   public
   ! -- modflow 6 version
   integer(I4B), parameter :: IDEVELOPMODE = 1
-  character(len=*), parameter :: VERSIONNUMBER = '6.6.1'
-  character(len=*), parameter :: VERSIONTAG = ' 02/10/2025'
+  character(len=*), parameter :: VERSIONNUMBER = '6.7.0.dev1'
+  character(len=*), parameter :: VERSIONTAG = ' (preliminary) 02/10/2025'
   character(len=40), parameter :: VERSION = VERSIONNUMBER//VERSIONTAG
   character(len=2), parameter :: MFVNAM = ' 6'
   character(len=*), parameter :: MFTITLE = &
@@ -75,18 +75,16 @@ module VersionModule
   ! -- disclaimer must be appropriate for version (release or release candidate)
   character(len=*), parameter :: FMTDISCLAIMER = &
     "(/,&
-    &'This software has been approved for release by the U.S. Geological ',/,&
-    &'Survey (USGS). Although the software has been subjected to rigorous ',/,&
-    &'review, the USGS reserves the right to update the software as needed ',/,&
-    &'pursuant to further analysis and review. No warranty, expressed or ',/,&
-    &'implied, is made by the USGS or the U.S. Government as to the ',/,&
-    &'functionality of the software and related material nor shall the ',/,&
-    &'fact of release constitute any such warranty. Furthermore, the ',/,&
-    &'software is released on condition that neither the USGS nor the U.S. ',/,&
-    &'Government shall be held liable for any damages resulting from its ',/,&
-    &'authorized or unauthorized use. Also refer to the USGS Water ',/,&
-    &'Resources Software User Rights Notice for complete use, copyright, ',/,&
-    &'and distribution information.',/)"
+    &'This software is preliminary or provisional and is subject to ',/,&
+    &'revision. It is being provided to meet the need for timely best ',/,&
+    &'science. The software has not received final approval by the U.S. ',/,&
+    &'Geological Survey (USGS). No warranty, expressed or implied, is made ',/,&
+    &'by the USGS or the U.S. Government as to the functionality of the ',/,&
+    &'software and related material nor shall the fact of release ',/,&
+    &'constitute any such warranty. The software is provided on the ',/,&
+    &'condition that neither the USGS nor the U.S. Government shall be held ',/,&
+    &'liable for any damages resulting from the authorized or unauthorized ',/,&
+    &'use of the software.',/)"
 
 contains
 
